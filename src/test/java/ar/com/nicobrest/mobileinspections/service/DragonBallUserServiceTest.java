8257d46a26f21431914db4c84fae9f8a8ee2cfe7
nbrest <brest.nico@gmail.com>
2016-02-18 12:35:55 -0300

    Added new attributes validation to the DragonBallUserServiceTest

--- src/test/java/ar/com/nicobrest/mobileinspections/service/DragonBallUserServiceTest.java
+++ src/test/java/ar/com/nicobrest/mobileinspections/service/DragonBallUserServiceTest.java
@@ -59,25 +59,34 @@ public class DragonBallUserServiceTest {
 
     // Create test data to be returned by mock object dragonBallUserServiceMock
     DragonBallUser user1 = new DragonBallUser();
+    user1.setId(1000L);
     user1.setAge(49);
     user1.setEmail("gokuTestMock@dbz.com");
     user1.setUsername("gokuTestMock");
+    user1.setPowerLevel(30);
+    user1.setStamina(1000);
     
     DragonBallUser user2 = new DragonBallUser();
+    user2.setId(1001L);
     user2.setAge(29);
     user2.setEmail("gohanTestMock@dbz.com");
     user2.setUsername("gohanTestMock");
+    user2.setPowerLevel(20);
+    user2.setStamina(1000);
     
     DragonBallUser user3 = new DragonBallUser();
+    user3.setId(1002L);
     user3.setAge(19);
     user3.setEmail("gotenTestMock@dbz.com");
     user3.setUsername("gotenTestMock");
+    user3.setPowerLevel(10);
+    user3.setStamina(1000);
     
     dragonBallUsers = new ArrayList<DragonBallUser>();
     dragonBallUsers.add(user1);
     dragonBallUsers.add(user2);
     dragonBallUsers.add(user3);
-  }  
+  }
 
   /**
    * @since v0.03
@@ -114,14 +123,23 @@ public class DragonBallUserServiceTest {
     assertEquals("gokuTestMock", usersList.get(0).getUsername());
     assertEquals("gokuTestMock@dbz.com", usersList.get(0).getEmail());
     assertEquals(49, usersList.get(0).getAge());
+    assertEquals("1000", usersList.get(0).getId().toString());
+    assertEquals(30, usersList.get(0).getPowerLevel());
+    assertEquals(1000, usersList.get(0).getStamina());
     
     assertEquals("gohanTestMock", usersList.get(1).getUsername());
     assertEquals("gohanTestMock@dbz.com", usersList.get(1).getEmail());
     assertEquals(29, usersList.get(1).getAge());
+    assertEquals("1001", usersList.get(1).getId().toString());
+    assertEquals(20, usersList.get(1).getPowerLevel());
+    assertEquals(1000, usersList.get(1).getStamina());
     
     assertEquals("gotenTestMock", usersList.get(2).getUsername());
     assertEquals("gotenTestMock@dbz.com", usersList.get(2).getEmail());
     assertEquals(19, usersList.get(2).getAge());
+    assertEquals("1002", usersList.get(2).getId().toString());
+    assertEquals(10, usersList.get(2).getPowerLevel());
+    assertEquals(1000, usersList.get(2).getStamina());
     
     verify(dragonBallUserDaoMock, times(1)).getAllDragonBallUsers();
   }
