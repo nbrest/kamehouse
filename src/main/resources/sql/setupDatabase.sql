64157c0bbafd86813cfa2dbebb001a64f7e30ef8
nbrest <brest.nico@gmail.com>
2016-07-15 12:21:42 -0300

    Updated database initialization script. Setup project to stop creating and droping database at application startup.

--- src/main/resources/sql/setupDatabase.sql
+++ src/main/resources/sql/setupDatabase.sql
@@ -1,3 +1,7 @@
+-- ********************************
+-- *** Last updated: 2016/07/15 ***
+-- ********************************
+
 -- Create schema:
 CREATE SCHEMA mobileInsp;
 -- Use schema:
@@ -14,5 +18,34 @@ DROP USER mobileInsp@localhost;
 -- Drop schema:
 DROP SCHEMA mobileInsp;
 
--- Query user permissions:
+-- Query user permissions in mysql:
 SELECT * FROM mysql.user;
+-- Export table creation script in mysql:
+show create table dragonballuser;
+show create table hibernate_sequence;
+
+-- MobileInspections Tables for mysql:
+DROP TABLE IF EXISTS dragonballuser;
+DROP TABLE IF EXISTS hibernate_sequence;
+
+CREATE TABLE dragonballuser (
+  id bigint(20) NOT NULL,
+  age int(11) DEFAULT NULL,
+  email varchar(255) DEFAULT NULL,
+  powerlevel int(11) DEFAULT NULL,
+  stamina int(11) DEFAULT NULL,
+  username varchar(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY UK_username (username)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE hibernate_sequence (
+   next_val bigint(20) DEFAULT NULL
+ ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Hibernate expects hibernate_sequence to have an initial value or it will fail
+INSERT INTO hibernate_sequence(next_val) VALUES (0);
+
+-- Query MobileInspections tables:
+SELECT * FROM dragonballuser;
+SELECT * FROM hibernate_sequence;
