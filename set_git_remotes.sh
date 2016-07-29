57bbd93414d4fcd20574d865828e4f7b7775886c
nbrest <brest.nico@gmail.com>
2016-07-28 23:47:28 -0300

    Added script to set remote repos

---
+++ set_git_remotes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+### Create the remote named all with fetch from the github repo
+git remote remove all
+git remote add all https://github.com/nbrest/java.web.mobile.inspections.git
+
+### Add the nicobrest repo to the remote all push
+git remote set-url --add --push all https://www.nicobrest.com.ar/git/java.web.mobile.inspections.git
+
+### Add the github repo to the remote all push
+git remote set-url --add --push all https://github.com/nbrest/java.web.mobile.inspections.git
+
+### Create a remote named github to be able to push only to github
+git remote remove github
+git remote add github https://github.com/nbrest/java.web.mobile.inspections.git
+
+### Create a remote named nicobrest to be able to push only to my private repo
+git remote remove nicobrest
+git remote add nicobrest https://nbrest@www.nicobrest.com.ar/git/my.scripts.git
+
+### list all the remotes
+git remote -v 
+
