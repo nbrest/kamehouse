362eb8bb61510687605c2375108bc80442d3ea9b
nbrest <brest.nico@gmail.com>
2017-02-23 01:47:41 +1100

    Updated remotes script

--- scripts/set_git_remotes.sh
+++ scripts/set_git_remotes.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-####################################################
-### REPOSITORY NAME: java.web.mobile.inspections ###
-####################################################
+
+REPOSITORY_NAME="java.web.mobile.inspections"
 
 #############
 # HTTPS all #
@@ -9,17 +8,17 @@
 
 ### Create the remote named all with fetch from the bitbucket repo
 git remote remove all
-git remote add all https://nbrest@bitbucket.org/nbrest/java.web.mobile.inspections.git
-#git remote add all https://github.com/nbrest/java.web.mobile.inspections.git
+git remote add all https://nbrest@bitbucket.org/nbrest/${REPOSITORY_NAME}.git
+#git remote add all https://nbrest@github.com/nbrest/${REPOSITORY_NAME}.git
 
 ### Add the bitbucket repo to the remote all push
-git remote set-url --add --push all https://nbrest@bitbucket.org/nbrest/java.web.mobile.inspections.git
+git remote set-url --add --push all https://nbrest@bitbucket.org/nbrest/${REPOSITORY_NAME}.git
 
 ### Add the github repo to the remote all push
-git remote set-url --add --push all https://github.com/nbrest/java.web.mobile.inspections.git
+git remote set-url --add --push all https://nbrest@github.com/nbrest/${REPOSITORY_NAME}.git
 
 ### Add the nicobrest repo to the remote all push
-#git remote set-url --add --push all https://www.nicobrest.com.ar/git/java.web.mobile.inspections.git
+#git remote set-url --add --push all https://nbrest@www.nicobrest.com.ar/git/${REPOSITORY_NAME}.git
 
 #########
 # HTTPS #
@@ -27,15 +26,15 @@ git remote set-url --add --push all https://github.com/nbrest/java.web.mobile.in
 
 ### Create a remote named bitbucket to be able to push only to bitbucket
 git remote remove bitbucket
-git remote add bitbucket https://nbrest@bitbucket.org/nbrest/java.web.mobile.inspections.git
+git remote add bitbucket https://nbrest@bitbucket.org/nbrest/${REPOSITORY_NAME}.git
 
 ### Create a remote named github to be able to push only to github
 git remote remove github
-git remote add github https://github.com/nbrest/java.web.mobile.inspections.git
+git remote add github https://nbrest@github.com/nbrest/${REPOSITORY_NAME}.git
 
 ### Create a remote named nicobrest to be able to push only to my private repo
-git remote remove nicobrest
-git remote add nicobrest https://www.nicobrest.com.ar/git/java.web.mobile.inspections.git
+#git remote remove nicobrest
+#git remote add nicobrest https://nbrest@www.nicobrest.com.ar/git/${REPOSITORY_NAME}.git
 
 #######
 # SSH #
@@ -43,15 +42,15 @@ git remote add nicobrest https://www.nicobrest.com.ar/git/java.web.mobile.inspec
 
 ### Create a remote named bitbucket to push using ssh to github
 git remote remove bitbucketssh
-git remote add bitbucketssh git@bitbucket.org:nbrest/java.web.mobile.inspections.git
+git remote add bitbucketssh git@bitbucket.org:nbrest/${REPOSITORY_NAME}.git
 
 ### Create a remote named githubssh to push using ssh to github
 git remote remove githubssh
-git remote add githubssh git@github.com:nbrest/java.web.mobile.inspections.git
+git remote add githubssh git@github.com:nbrest/${REPOSITORY_NAME}.git
 
 ### Create a remote to push using ssh inside my local network without internet access
-git remote remove localssh
-git remote add localssh ssh://nbrest@niko-w/git/java.web.mobile.inspections.git
+#git remote remove localssh
+#git remote add localssh ssh://nbrest@niko-w/git/${REPOSITORY_NAME}.git
 
 ### list all the remotes
 git remote -v 
