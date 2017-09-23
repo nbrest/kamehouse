9d8aa3f3c8a2ac75f0c4a1b85a38da5eeb41e29d
nbrest <brest.nico@gmail.com>
2017-09-23 11:57:53 +1000

    Fixed bug of null pointer exception when getting the dragonBallUserService bean

--- src/main/java/com/nicobrest/kamehouse/servlet/DragonBallUserAddActionServlet.java
+++ src/main/java/com/nicobrest/kamehouse/servlet/DragonBallUserAddActionServlet.java
@@ -2,9 +2,9 @@ package com.nicobrest.kamehouse.servlet;
 
 import com.nicobrest.kamehouse.model.DragonBallUser;
 import com.nicobrest.kamehouse.service.DragonBallUserService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+ 
+import org.springframework.context.ApplicationContext; 
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import java.io.IOException;
 
@@ -17,6 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet to process requests to add a DragonBallUser from the repository.
+ * Ideally I would use a spring controller instead of a raw Servlet but I'm
+ * using Servlets for the JSPs just for practice.
  * 
  * @author nbrest
  *
@@ -26,27 +28,31 @@ public class DragonBallUserAddActionServlet extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
 
-  @Autowired
   private static DragonBallUserService dragonBallUserService;
 
   public static void setDragonBallUserService(DragonBallUserService dragonBallUserService) {
-
     DragonBallUserAddActionServlet.dragonBallUserService = dragonBallUserService;
   }
-
+  
   public static DragonBallUserService getDragonBallUserService() {
-
     return DragonBallUserAddActionServlet.dragonBallUserService;
   }
 
   /**
-   * Configures spring context.
+   * Configures private static dragonBallUserService. @Autowired doesn't work
+   * because the servlet is not managed by spring and the initialization of
+   * static fields probably happens before the spring context loads. The only
+   * way for @Autowired to work was to have the property non-static and use
+   * SpringBeanAutowiringSupport in the init method.
    */
   @Override
   public void init(ServletConfig config) throws ServletException {
     super.init(config);
-    SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, config
+    ApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(this
         .getServletContext());
+    DragonBallUserService dragonBallUserService = (DragonBallUserService) context.getBean(
+        "dragonBallUserService");
+    setDragonBallUserService(dragonBallUserService);
   }
 
   /**
@@ -67,5 +73,4 @@ public class DragonBallUserAddActionServlet extends HttpServlet {
     getDragonBallUserService().createDragonBallUser(dragonBallUser);
     response.sendRedirect("users-list");
   }
-
 }
