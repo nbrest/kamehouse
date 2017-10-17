af2debed348aabd47f29e1e52e8a3918a60f9008
nbrest <brest.nico@gmail.com>
2017-10-18 00:21:08 +1100

    Removed unused imports

--- src/test/java/com/nicobrest/kamehouse/dao/DragonBallUserDaoJpaTest.java
+++ src/test/java/com/nicobrest/kamehouse/dao/DragonBallUserDaoJpaTest.java
@@ -15,8 +15,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
