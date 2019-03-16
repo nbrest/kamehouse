ec36a57f744d682ce6325ea8cabe02fe02db4eab
nbrest <brest.nico@gmail.com>
2019-03-17 02:22:42 +1100

    Test example to execute a system command

---
+++ src/main/java/com/nicobrest/kamehouse/sysadmin/controller/SysAdminController.java
@@ -0,0 +1,53 @@
+package com.nicobrest.kamehouse.sysadmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
+@Controller
+@RequestMapping(value = "/api/v1/sysadmin")
+public class SysAdminController {
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+
+    System.out.println("Test executing command start");
+
+    boolean isWindowsOS = System.getProperty("os.name").toLowerCase().startsWith("windows");
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (isWindowsOS) {
+      processBuilder.command("cmd.exe", "/c", "dir");
+    } else {
+      processBuilder.command("sh", "-c", "ls");
+    }
+    processBuilder.directory(new File(System.getProperty("user.home")));
+    Process process = processBuilder.start();
+    StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+    Executors.newSingleThreadExecutor().submit(streamGobbler);
+    int exitCode = process.waitFor();
+    assert exitCode == 0;
+
+    System.out.println("Test executing command end");
+  }
+
+  private static class StreamGobbler implements Runnable {
+    private InputStream inputStream;
+    private Consumer<String> consumer;
+
+    public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+      this.inputStream = inputStream;
+      this.consumer = consumer;
+    }
+
+    @Override
+    public void run() {
+      new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(consumer);
+    }
+  }
+}
