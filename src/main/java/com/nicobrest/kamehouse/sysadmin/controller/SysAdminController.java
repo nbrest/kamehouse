5316ec0e37a847e36997f4e462b19e80f61ea1c9
nbrest <brest.nico@gmail.com>
2019-04-25 13:24:37 +1000

    Added test command to get the status of running vlc process

--- src/main/java/com/nicobrest/kamehouse/sysadmin/controller/SysAdminController.java
+++ src/main/java/com/nicobrest/kamehouse/sysadmin/controller/SysAdminController.java
@@ -1,5 +1,7 @@
 package com.nicobrest.kamehouse.sysadmin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -7,21 +9,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/api/v1/sysadmin")
 public class SysAdminController {
 
+  private static final Logger logger = LoggerFactory.getLogger(SysAdminController.class);
+
   /**
-   * Test method to start a vlc player. 
+   * Test method to start a vlc player.
    */
   @RequestMapping(value = "/vlc-player-start", method = RequestMethod.GET)
   @ResponseBody
   public ResponseEntity<String> startVlcPlayer() throws IOException {
 
-    System.out.println("begin start vlc player");
+    logger.debug("begin start vlc player");
 
     boolean isWindowsOpSys = System.getProperty("os.name").toLowerCase(Locale.getDefault())
         .startsWith("windows");
@@ -32,20 +44,20 @@ public class SysAdminController {
     } else {
       processBuilder.command("vlc", "/home/nbrest/Videos/lleyton.hewitt.m3u");
     }
-    System.out.println("processBuilder.command()" + processBuilder.command().toString());
+    logger.debug("processBuilder.command()" + processBuilder.command().toString());
     processBuilder.start();
-    System.out.println("finish start vlc player");
+    logger.debug("finish start vlc player");
     return new ResponseEntity<String>("Started vlc player", HttpStatus.OK);
   }
 
   /**
-   * Test method to stop vlc player. 
+   * Test method to stop vlc player.
    */
   @RequestMapping(value = "/vlc-player-stop", method = RequestMethod.GET)
   @ResponseBody
   public ResponseEntity<String> stopVlcPlayer() throws IOException {
 
-    System.out.println("begin stop vlc player");
+    logger.debug("begin stop vlc player");
 
     boolean isWindowsOpSys = System.getProperty("os.name").toLowerCase(Locale.getDefault())
         .startsWith("windows");
@@ -55,10 +67,49 @@ public class SysAdminController {
     } else {
       processBuilder.command("skill", "-9", "vlc");
     }
-    System.out.println("processBuilder.command()" + processBuilder.command().toString());
+    logger.debug("processBuilder.command()" + processBuilder.command().toString());
     processBuilder.start();
-    System.out.println("finish stop vlc player");
+    logger.debug("finish stop vlc player");
     return new ResponseEntity<String>("Stopped vlc player", HttpStatus.OK);
+  }
+
+  /**
+   * Test method to get the status of vlc player.
+   */
+  @RequestMapping(value = "/vlc-player-status", method = RequestMethod.GET)
+  @ResponseBody
+  public ResponseEntity<Map<String, Object>> statusVlcPlayer() throws IOException,
+      InterruptedException {
+
+    logger.debug("begin status vlc player");
+
+    boolean isWindowsOpSys = System.getProperty("os.name").toLowerCase(Locale.getDefault())
+        .startsWith("windows");
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (isWindowsOpSys) {
+      processBuilder.command("tasklist", "/FI", "IMAGENAME eq vlc.exe");
+    } else {
+      processBuilder.command("ps", "aux", "|", "grep", "-e", "vlc\\|COMMAND", "|", "grep", "-v",
+          "grep");
+    }
+    logger.debug("processBuilder.command()" + processBuilder.command().toString());
+    Process process = processBuilder.start();
+    process.waitFor();
+    InputStream processInputStream = process.getInputStream();
+    BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(
+        processInputStream));
+    String inputStreamLine;
+    List<String> processOuputList = new ArrayList<String>();
+    while ((inputStreamLine = inputStreamReader.readLine()) != null) {
+      logger.info("inputStreamLine : " + inputStreamLine);
+      processOuputList.add(inputStreamLine);
+    }
+
+    Map<String, Object> processOutput = new HashMap<String, Object>();
+    processOutput.put("exitStatus", process.exitValue());
+    processOutput.put("output", processOuputList);
+    logger.debug("finish status vlc player");
+    return new ResponseEntity<Map<String, Object>>(processOutput, HttpStatus.OK);
   }
 
 }
