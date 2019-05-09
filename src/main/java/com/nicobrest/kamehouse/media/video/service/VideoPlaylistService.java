f41acefa821f67d12c776070f4ab2e55a590f760
nbrest <brest.nico@gmail.com>
2019-05-09 23:49:26 +1000

    Removed unused import

--- src/main/java/com/nicobrest/kamehouse/media/video/service/VideoPlaylistService.java
+++ src/main/java/com/nicobrest/kamehouse/media/video/service/VideoPlaylistService.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
