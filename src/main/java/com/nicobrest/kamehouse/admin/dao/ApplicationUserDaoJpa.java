3d0d06a686d57a82f0425119e9a13110ec89d49e
nbrest <brest.nico@gmail.com>
2019-09-05 20:05:13 +1000

    Fixed sonarcube reported new issues with logging and throwing exceptions.

--- src/main/java/com/nicobrest/kamehouse/admin/dao/ApplicationUserDaoJpa.java
+++ src/main/java/com/nicobrest/kamehouse/admin/dao/ApplicationUserDaoJpa.java
@@ -59,7 +59,6 @@ public class ApplicationUserDaoJpa implements ApplicationUserDao {
       em.merge(applicationUser);
       em.getTransaction().commit();
     } catch (PersistenceException pe) {
-      logger.error("PersistenceException", pe);
       // Iterate through the causes of the PersistenceException to identify and
       // return the correct exception.
       Throwable cause = pe;
@@ -95,7 +94,6 @@ public class ApplicationUserDaoJpa implements ApplicationUserDao {
       applicationUser = (ApplicationUser) queryAppUser.getSingleResult();
       em.getTransaction().commit();
     } catch (PersistenceException pe) {
-      logger.error("PersistenceException", pe);
       // Iterate through the causes of the PersistenceException to identify and
       // return the correct exception.
       Throwable cause = pe;
@@ -149,7 +147,6 @@ public class ApplicationUserDaoJpa implements ApplicationUserDao {
             + " was not found in the repository.");
       }
     } catch (PersistenceException pe) {
-      logger.error("PersistenceException", pe);
       // Iterate through the causes of the PersistenceException to identify and
       // return the correct exception.
       Throwable cause = pe;
@@ -185,7 +182,6 @@ public class ApplicationUserDaoJpa implements ApplicationUserDao {
             + " was not found in the repository.");
       }
     } catch (PersistenceException pe) {
-      logger.error("PersistenceException", pe);
       throw new KameHouseServerErrorException("PersistenceException in deleteUser", pe);
     } finally {
       em.close();
@@ -205,7 +201,6 @@ public class ApplicationUserDaoJpa implements ApplicationUserDao {
           .getResultList();
       em.getTransaction().commit();
     } catch (PersistenceException pe) {
-      logger.error("PersistenceException", pe);
       throw new KameHouseServerErrorException("PersistenceException in getAllUsers", pe);
     } finally {
       em.close();
