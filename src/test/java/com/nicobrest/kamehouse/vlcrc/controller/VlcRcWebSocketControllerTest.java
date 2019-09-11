a4972d8de79b99c06621acb20b66e4cd631a0506
nbrest <brest.nico@gmail.com>
2019-09-11 12:25:25 +1000

    Updated Spring version to 4.3.25.RELEASE, Spring security to 4.2.13.RELEASE and other dependencies. Added unit tests for VlcRcWebSocketController.

---
+++ src/test/java/com/nicobrest/kamehouse/vlcrc/controller/VlcRcWebSocketControllerTest.java
@@ -0,0 +1,199 @@
+package com.nicobrest.kamehouse.vlcrc.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.nicobrest.kamehouse.vlcrc.model.VlcRcStatus;
+import com.nicobrest.kamehouse.vlcrc.model.VlcRcStatus.Equalizer;
+import com.nicobrest.kamehouse.vlcrc.model.VlcRcStatus.Information;
+import com.nicobrest.kamehouse.vlcrc.service.VlcRcService;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Test class for the VlcRcWebSocketController.
+ * 
+ * @author nicolas.brest
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+public class VlcRcWebSocketControllerTest {
+
+  private static VlcRcStatus vlcRcStatusMock;
+
+  @InjectMocks
+  private VlcRcWebSocketController vlcRcWebSocketController;
+
+  @Mock(name = "vlcRcService")
+  private VlcRcService vlcRcServiceMock;
+
+  @BeforeClass
+  public static void beforeClassTest() {
+
+    vlcRcStatusMock = new VlcRcStatus();
+    // Direct attributes
+    vlcRcStatusMock.setApiVersion(3);
+    vlcRcStatusMock.setAspectRatio("16:9");
+    vlcRcStatusMock.setAudioDelay(1);
+    vlcRcStatusMock.setCurrentPlId(1);
+    vlcRcStatusMock.setFullscreen(true);
+    vlcRcStatusMock.setLength(1000);
+    vlcRcStatusMock.setLoop(false);
+    vlcRcStatusMock.setPosition(0.1);
+    vlcRcStatusMock.setRandom(false);
+    vlcRcStatusMock.setRate(1);
+    vlcRcStatusMock.setRepeat(false);
+    vlcRcStatusMock.setState("playing");
+    vlcRcStatusMock.setSubtitleDelay(1);
+    vlcRcStatusMock.setTime(1);
+    vlcRcStatusMock.setVersion("yukimura");
+    vlcRcStatusMock.setVolume(1);
+    // audio filters
+    Map<String, String> audioFilters = new HashMap<String, String>();
+    audioFilters.put("filter_0", "");
+    vlcRcStatusMock.setAudioFilters(audioFilters);
+    // video effects
+    Map<String, Integer> videoEffects = new HashMap<String, Integer>();
+    videoEffects.put("saturation", 1);
+    videoEffects.put("brightness", 1);
+    videoEffects.put("contrast", 1);
+    videoEffects.put("hue", 0);
+    videoEffects.put("gamma", 1);
+    vlcRcStatusMock.setVideoEffects(videoEffects);
+    // stats
+    Map<String, Object> stats = new HashMap<String, Object>();
+    stats.put("inputBitrate", 1);
+    stats.put("sentBytes", 1);
+    stats.put("lostaBuffers", 1);
+    stats.put("averageDemuxBitrate", 1);
+    stats.put("readPackets", 1);
+    stats.put("demuxReadPackets", 1);
+    stats.put("lostPictures", 1);
+    stats.put("displayedPictures", 1);
+    stats.put("sentPackets", 1);
+    stats.put("demuxReadBytes", 1);
+    stats.put("demuxBitrate", 1);
+    stats.put("playedaBuffers", 1);
+    stats.put("demuxDiscontinuity", 1);
+    stats.put("decodedAudio", 1);
+    stats.put("sendBitrate", 1);
+    stats.put("readBytes", 1);
+    stats.put("averageInputBitrate", 1);
+    stats.put("demuxCorrupted", 1);
+    stats.put("decodedVideo", 1);
+    vlcRcStatusMock.setStats(stats);
+    // equalizer
+    Equalizer equalizer = new VlcRcStatus.Equalizer();
+    equalizer.setPreAmp(1);
+    vlcRcStatusMock.setEqualizer(equalizer);
+    // information
+    Information information = new VlcRcStatus.Information();
+    information.setChapter("0");
+    information.setChapters(Arrays.asList(""));
+    information.setChapter("0");
+    information.setChapters(Arrays.asList(""));
+    List<Map<String, Object>> informationCategories = new ArrayList<Map<String, Object>>();
+    Map<String, Object> meta = new HashMap<String, Object>();
+    meta.put("name", "goku-name");
+    meta.put("filename", "goku-filename");
+    meta.put("title", "goku-title");
+    meta.put("artist", "goku-artist");
+    meta.put("setting", "goku-setting");
+    meta.put("software", "goku-software");
+    informationCategories.add(meta);
+    Map<String, Object> video = new HashMap<String, Object>();
+    video.put("name", "goku-name");
+    video.put("type", "goku-type");
+    video.put("frameRate", "goku-frameRate");
+    video.put("decodedFormat", "goku-decodedFormat");
+    video.put("displayResolution", "goku-displayResolution");
+    video.put("codec", "goku-codec");
+    video.put("language", "goku-language");
+    video.put("resolution", "goku-resolution");
+    informationCategories.add(video);
+    Map<String, Object> audio = new HashMap<String, Object>();
+    audio.put("name", "goku-name");
+    audio.put("type", "goku-type");
+    audio.put("bitrate", "goku-bitrate");
+    audio.put("channels", "goku-channels");
+    audio.put("sampleRate", "goku-sampleRate");
+    audio.put("codec", "goku-codec");
+    audio.put("language", "goku-language");
+    informationCategories.add(audio);
+    Map<String, Object> subtitle = new HashMap<String, Object>();
+    subtitle.put("name", "goku-name");
+    subtitle.put("type", "goku-type");
+    subtitle.put("codec", "goku-codec");
+    subtitle.put("language", "goku-language");
+    informationCategories.add(subtitle);
+    information.setCategory(informationCategories);
+    vlcRcStatusMock.setInformation(information);
+  }
+
+  @Before
+  public void beforeTest() {
+    MockitoAnnotations.initMocks(this);
+    Mockito.reset(vlcRcServiceMock);
+  }
+
+  /**
+   * Tests getting VlcRcStatus.
+   */
+  @Test
+  public void getVlcRcStatusTest() {
+
+    try {
+      when(vlcRcServiceMock.getVlcRcStatus("localhost")).thenReturn(vlcRcStatusMock);
+
+      VlcRcStatus returnedVlcRcStatus = vlcRcWebSocketController.getVlcRcStatus();
+
+      verify(vlcRcServiceMock, times(1)).getVlcRcStatus("localhost");
+      assertEquals(vlcRcStatusMock, returnedVlcRcStatus);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Unexpected exception thrown.");
+    }
+  }
+
+  /**
+   * Tests getting VlcRcStatus when VlcRcService returns null.
+   */
+  @Test
+  public void getVlcRcStatusNullTest() {
+
+    try {
+      when(vlcRcServiceMock.getVlcRcStatus("localhost")).thenReturn(null);
+      VlcRcStatus emptyVlcRcStatus = new VlcRcStatus();
+
+      VlcRcStatus returnedVlcRcStatus = vlcRcWebSocketController.getVlcRcStatus();
+
+      verify(vlcRcServiceMock, times(1)).getVlcRcStatus("localhost");
+      assertEquals(emptyVlcRcStatus.getInformation(), returnedVlcRcStatus.getInformation());
+      assertEquals(emptyVlcRcStatus.getAspectRatio(), returnedVlcRcStatus.getAspectRatio());
+      assertEquals(emptyVlcRcStatus.getFullscreen(), returnedVlcRcStatus.getFullscreen());
+      assertEquals(emptyVlcRcStatus.getApiVersion(), returnedVlcRcStatus.getApiVersion());
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Unexpected exception thrown.");
+    }
+  }
+}
