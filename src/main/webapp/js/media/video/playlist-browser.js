19fd33b5bb033c4bcfd2944d55154934a7b405a6
nbrest <brest.nico@gmail.com>
2020-06-28 10:51:13 +1000

    Updated playlist and playlist browser styles

--- src/main/webapp/js/media/video/playlist-browser.js
+++ src/main/webapp/js/media/video/playlist-browser.js
@@ -113,7 +113,7 @@ function PlaylistBrowser(vlcPlayer) {
     } else {
       for (let i = 0; i < self.currentPlaylist.files.length; i++) {
         let playlistElementButton = $('<button>');
-        playlistElementButton.addClass("playlist-table-btn");
+        playlistElementButton.addClass("playlist-browser-table-btn");
         let filename = self.currentPlaylist.files[i];
         playlistElementButton.data("filename", filename);
         playlistElementButton.text(filename);
