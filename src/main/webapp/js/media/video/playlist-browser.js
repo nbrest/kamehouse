29926f6d2be8fe3bc95c10aeac158299a807ebfa
nbrest <brest.nico@gmail.com>
2020-08-04 00:52:17 +1000

    Improved the performance of toggle expand/collapse filenames for playlist browser A LOT by changing completely the way it's done. Now it keeps an in memory version of each and replaces the entire tbody at once instead of individually updating each row. Need to do the same for the main playlist

--- src/main/webapp/js/media/video/playlist-browser.js
+++ src/main/webapp/js/media/video/playlist-browser.js
@@ -15,6 +15,8 @@ function PlaylistBrowser(vlcPlayer) {
   this.currentPlaylist = null;
   const mediaVideoAllPlaylistsUrl = '/kame-house/api/v1/media/video/playlists';
   const mediaVideoPlaylistUrl = '/kame-house/api/v1/media/video/playlist';
+  this.tbodyAbsolutePaths = null;
+  this.tbodyFilenames = null;
 
   /** Filter playlist browser rows based on the search string. */
   this.filterPlaylistRows = (filterString) => tableUtils.filterTableRows(filterString, 'playlist-browser-table-body');
@@ -103,26 +105,39 @@ function PlaylistBrowser(vlcPlayer) {
     // Clear playlist browser table content. 
     $("#playlist-browser-table-body").empty();
     // Add the new playlist browser items received from the server.
-    let $playlistTableBody = $('#playlist-browser-table-body');
-    let playlistTableRow;
+    let $playlistTableBody = $('#playlist-browser-table-body');    
+    self.tbodyFilenames = $('<tbody id="playlist-browser-table-body">');
+    self.tbodyAbsolutePaths = $('<tbody id="playlist-browser-table-body">');
+
     if (isNullOrUndefined(self.currentPlaylist)) {
       playlistTableRow = $('<tr>').append($('<td>').text("No playlist to browse loaded yet or unable to sync. まだまだだね :)"));
       $playlistTableBody.append(playlistTableRow);
     } else {
       for (let i = 0; i < self.currentPlaylist.files.length; i++) {
-        let playlistElementButton = $('<button>');
-        playlistElementButton.addClass("playlist-browser-table-btn");
         let absolutePath = self.currentPlaylist.files[i];
         let filename = fileUtils.getShortFilename(absolutePath);
-        playlistElementButton.data("filename", filename);
-        playlistElementButton.data("absolutePath", absolutePath);
-        playlistElementButton.text(filename);
-        playlistElementButton.click({
+
+        // Create the in-memory tbody of filenames to toggle with absolute paths
+        let playlistElementButtonFilenames = $('<button>');
+        playlistElementButtonFilenames.addClass("playlist-browser-table-btn");
+        playlistElementButtonFilenames.text(filename);
+        playlistElementButtonFilenames.click({
           filename: absolutePath
         }, self.clickEventOnPlaylistBrowserRow);
-        playlistTableRow = $('<tr id=playlist-browser-entry-' + [i] + '>').append($('<td>').append(playlistElementButton));
-        $playlistTableBody.append(playlistTableRow);
+        let playlistTableRowFilenames = $('<tr id=playlist-browser-entry-' + [i] + '>').append($('<td>').append(playlistElementButtonFilenames));
+        self.tbodyFilenames.append(playlistTableRowFilenames);
+
+        // Create the in-memory tbody of absolute paths to toggle with filenames
+        let playlistElementButtonAbsolutePaths = $('<button>');
+        playlistElementButtonAbsolutePaths.addClass("playlist-browser-table-btn");  
+        playlistElementButtonAbsolutePaths.text(absolutePath);
+        playlistElementButtonAbsolutePaths.click({
+          filename: absolutePath
+        }, self.clickEventOnPlaylistBrowserRow);
+        let playlistTableRowAbsolutePaths = $('<tr id=playlist-browser-entry-' + [i] + '>').append($('<td>').append(playlistElementButtonAbsolutePaths));
+        self.tbodyAbsolutePaths.append(playlistTableRowAbsolutePaths);
       }
+      $playlistTableBody.replaceWith(self.tbodyFilenames);
     }
   }
 
@@ -138,21 +153,21 @@ function PlaylistBrowser(vlcPlayer) {
   this.toggleExpandPlaylistFilenames = function toggleExpandPlaylistFilenames() {
     logger.debugFunctionCall();
     let isExpandedFilename = true;
-    $('#playlist-browser-table-body tr').each(function () {
-      let playlistEntry = $(this).children().children();
-      let filename = playlistEntry.data("filename");
-      let currentText = playlistEntry.text();
-      if (currentText == filename) {
-        // Currently it's showing the short filename. Update to expanded absolute path
-        let absolutePath = playlistEntry.data("absolutePath");
-        playlistEntry.text(absolutePath);
-        isExpandedFilename = true;
-      } else {
-        // Currently it's showing the expanded absolute path. Update to short filename
-        playlistEntry.text(filename);
-        isExpandedFilename = false;
-      }
-    });
+    let filenamesFirstFile = $(self.tbodyFilenames).children().first().text();
+    let currentFirstFile = $('#playlist-browser-table-body tr:first').text();
+    let $playlistTable = $('#playlist-browser-table');
+
+    if (currentFirstFile == filenamesFirstFile) {
+      // currently displaying filenames, switch to absolute paths 
+      self.tbodyFilenames.detach();
+      $playlistTable.append(self.tbodyAbsolutePaths);
+      isExpandedFilename = true;
+    } else {
+      // currently displaying absolute paths, switch to filenames 
+      self.tbodyAbsolutePaths.detach();  
+      $playlistTable.append(self.tbodyFilenames);
+      isExpandedFilename = false;
+    }
     self.updateExpandPlaylistFilenamesIcon(isExpandedFilename);
   }
 
