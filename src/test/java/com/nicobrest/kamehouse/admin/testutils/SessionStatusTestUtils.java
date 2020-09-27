ea927fc1375ab36276d0f8695c53e22c2ed90d69
nbrest <brest.nico@gmail.com>
2020-09-27 17:09:40 +1000

    Updated session status test utils

--- src/test/java/com/nicobrest/kamehouse/admin/testutils/SessionStatusTestUtils.java
+++ src/test/java/com/nicobrest/kamehouse/admin/testutils/SessionStatusTestUtils.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import com.nicobrest.kamehouse.admin.model.SessionStatus;
 import com.nicobrest.kamehouse.main.testutils.AbstractTestUtils;
 import com.nicobrest.kamehouse.main.testutils.TestUtils;
+import com.nicobrest.kamehouse.main.utils.PropertiesUtils;
 
 /**
  * Test data and common test methods to test SessionStatus in all layers of
@@ -27,10 +28,12 @@ public class SessionStatusTestUtils extends AbstractTestUtils<SessionStatus, Obj
     assertEquals(expected.getUsername(), returned.getUsername());
     assertEquals(expected.getFirstName(), returned.getFirstName());
     assertEquals(expected.getLastName(), returned.getLastName());
+    assertEquals(expected.getServer(), returned.getServer());
   }
 
   private void initSingleTestData() {
     singleTestData = new SessionStatus();
     singleTestData.setUsername("anonymousUser");
+    singleTestData.setServer(PropertiesUtils.getHostname());
   }
 }
