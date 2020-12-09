7b28f748e742c379ae45c6085bc185bb64352b49
nbrest <brest.nico@gmail.com>
2020-12-09 20:38:13 +1100

    Updated date utils test to work on any timezone

--- src/test/java/com/nicobrest/kamehouse/main/utils/DateUtilsTest.java
+++ src/test/java/com/nicobrest/kamehouse/main/utils/DateUtilsTest.java
@@ -1,7 +1,11 @@
 package com.nicobrest.kamehouse.main.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Matchers;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -39,6 +43,9 @@ public class DateUtilsTest {
   public void addSecondsTest() {
     Date date = new GregorianCalendar(2020, Calendar.OCTOBER, 15).getTime();
     Date output = DateUtils.addSeconds(date, 60);
-    assertEquals("Thu Oct 15 00:01:00 AEDT 2020", output.toString());
+    // format: Xxx Xxx 99 99:99:99 XXXX 9999
+    String expectedDateRegex = "[A-Za-z]{3} [A-Za-z]{3} [0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2} " +
+        "[A-Za-z]+ [0-9]{4}";
+    assertTrue("Date doesn't match expected format", output.toString().matches(expectedDateRegex));
   }
 }
