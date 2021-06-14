4c8844490b3ccd9537f59771406e19d7a8e9b104
nbrest <brest.nico@gmail.com>
2021-06-14 12:44:31 +1000

    - Angular views to angular subfolder

--- kamehouse-ui/src/main/webapp/js/test-module/angular-1/app.js
+++ kamehouse-ui/src/main/webapp/js/test-module/angular-1/app.js
@@ -13,28 +13,28 @@ var App = angular.module('myApp', [ "ngRoute" ]);
 App.config(function($routeProvider, $locationProvider) {
   $routeProvider
     .when("/", {
-      templateUrl : "/kame-house/view/test-module/angular-1/home.html"
+      templateUrl: "/kame-house/test-module/angular-1/view/home.html"
     })
     .when("/dragonball/users", {
-      templateUrl : "/kame-house/view/test-module/angular-1/dragonball-users.html"
+      templateUrl: "/kame-house/test-module/angular-1/view/dragonball-users.html"
     })
     .when("/400", {
-      templateUrl : "/kame-house/view/test-module/angular-1/400.html"
+      templateUrl: "/kame-house/test-module/angular-1/view/400.html"
     })
     .when("/403", {
-      templateUrl : "/kame-house/view/test-module/angular-1/403.html"
+      templateUrl: "/kame-house/test-module/angular-1/view/403.html"
     })
     .when("/405", {
-      templateUrl : "/kame-house/view/test-module/angular-1/405.html"
+      templateUrl: "/kame-house/test-module/angular-1/view/405.html"
     })
     .when("/409", {
-      templateUrl : "/kame-house/view/test-module/angular-1/409.html"
+      templateUrl: "/kame-house/test-module/angular-1/view/409.html"
     })
     .when("/500", {
-      templateUrl : "/kame-house/view/test-module/angular-1/500.html"
+      templateUrl: "/kame-house/test-module/angular-1/view/500.html"
     })
     .otherwise({
-      templateUrl : "/kame-house/view/test-module/angular-1/404.html"
+      templateUrl: "/kame-house/test-module/angular-1/view/404.html"
     });
 
   $locationProvider.hashPrefix('');
