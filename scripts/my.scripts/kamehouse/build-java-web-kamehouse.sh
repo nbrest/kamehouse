e743ef080a92387c84a6f069a49fd0a409bdc846
nbrest <brest.nico@gmail.com>
2021-07-25 13:03:29 +1000

    Exported updates to my.scripts

--- scripts/my.scripts/kamehouse/build-java-web-kamehouse.sh
+++ scripts/my.scripts/kamehouse/build-java-web-kamehouse.sh
@@ -65,7 +65,7 @@ printHelp() {
   echo -e ""
   echo -e "  Options:"  
   echo -e "     ${COL_BLUE}-h${COL_NORMAL} display help" 
-  echo -e "     ${COL_BLUE}-m (admin|cmd|media|tennisworld|testmodule|ui|vlcrc)${COL_NORMAL} module to build"
+  echo -e "     ${COL_BLUE}-m (admin|cmd|groot|media|tennisworld|testmodule|ui|vlcrc)${COL_NORMAL} module to build"
   echo -e "     ${COL_BLUE}-p (prod|qa|dev)${COL_NORMAL} maven profile to build the project with. Default is prod if not specified"
 }
 
