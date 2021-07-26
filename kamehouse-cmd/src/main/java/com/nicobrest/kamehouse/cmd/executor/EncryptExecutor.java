ba49e194241dc9e66213c490c2abcd8d10808539
nbrest <brest.nico@gmail.com>
2021-07-26 23:42:14 +1000

    Added encrypt operation to kamehouse-cmd

--- kamehouse-cmd/src/main/java/com/nicobrest/kamehouse/cmd/executor/EncryptExecutor.java
+++ kamehouse-cmd/src/main/java/com/nicobrest/kamehouse/cmd/executor/EncryptExecutor.java
@@ -1,9 +1,17 @@
 package com.nicobrest.kamehouse.cmd.executor;
 
 import com.nicobrest.kamehouse.cmd.model.CmdArgumentHandler;
+import com.nicobrest.kamehouse.commons.utils.EncryptionUtils;
+import com.nicobrest.kamehouse.commons.utils.FileUtils;
+import com.nicobrest.kamehouse.commons.utils.PropertiesUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.cert.X509Certificate;
 
 /**
  * Executor for the encrypt operation.
@@ -19,8 +27,19 @@ public class EncryptExecutor {
    * Execute the operation.
    */
   public void execute(CmdArgumentHandler cmdArgumentHandler) {
-    String inputFile = cmdArgumentHandler.getArgument("if");
-    String outputFile = cmdArgumentHandler.getArgument("of");
-    logger.info("Encrypting contents of {} into {}", inputFile, outputFile);
+    String inputFileName = cmdArgumentHandler.getArgument("if");
+    String outputFileName = cmdArgumentHandler.getArgument("of");
+    try {
+      logger.info("Encrypting contents of {} into {}", inputFileName, outputFileName);
+      File inputFile = new File(inputFileName);
+      File outputFile = new File(outputFileName);
+      byte[] inputBytes = FileUtils.readFileToByteArray(inputFile);
+      X509Certificate cert = EncryptionUtils.getKameHouseCertificate();
+      byte[] encryptedBytes = EncryptionUtils.encrypt(inputBytes, cert);
+      FileUtils.writeByteArrayToFile(outputFile, encryptedBytes);
+      logger.info("Finished encrypting content");
+    } catch (IOException e) {
+      logger.error("Error encrypting file", e);
+    }
   }
 }
