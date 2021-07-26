ca3175b44593df8b61b064ae5332a65072356e73
nbrest <brest.nico@gmail.com>
2021-07-27 00:01:59 +1000

    Updated test resources for kamehouse-cmd

--- kamehouse-cmd/src/test/java/com/nicobrest/kamehouse/cmd/model/CmdArgumentHandlerTest.java
+++ kamehouse-cmd/src/test/java/com/nicobrest/kamehouse/cmd/model/CmdArgumentHandlerTest.java
@@ -3,8 +3,16 @@ package com.nicobrest.kamehouse.cmd.model;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
+/**
+ * Unit tests for the CmdArgumentHandler.
+ *
+ * @author nbrest
+ */
 public class CmdArgumentHandlerTest {
 
+  /**
+   * Tests parsing the arguments successfully.
+   */
   @Test
   public void cmdArgumentParserSuccessfulTest() {
     String[] args = new String[] { "-o", "encrypt", "-if", "in.txt", "-of", "out.enc"};
