6a4975e1faa48dd8c346883e4f905b417b375f4d
nbrest <brest.nico@gmail.com>
2021-08-28 02:09:45 +1000

    Added sort type to select column

--- kamehouse-ui/src/main/webapp/js/tennisworld/booking-request.js
+++ kamehouse-ui/src/main/webapp/js/tennisworld/booking-request.js
@@ -42,7 +42,8 @@ window.onload = () => {
           name: "duration",
           type: "select",
           values: ["0", "15", "30", "45", "60", "90", "120", "150", "180"],
-          displayValues: ["0", "15", "30", "45", "60", "90", "120", "150", "180"]
+          displayValues: ["0", "15", "30", "45", "60", "90", "120", "150", "180"],
+          sortType: "number"
         },
         { 
           name: "dryRun",
