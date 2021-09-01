a65fa80e41f06f27c02b105319272912673a36c9
nbrest <brest.nico@gmail.com>
2021-09-01 23:27:53 +1000

    Fixed spotbugs

--- kamehouse-commons-core/src/main/java/com/nicobrest/kamehouse/commons/config/DataSourceConfig.java
+++ kamehouse-commons-core/src/main/java/com/nicobrest/kamehouse/commons/config/DataSourceConfig.java
@@ -1,6 +1,5 @@
 package com.nicobrest.kamehouse.commons.config;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,13 +29,12 @@ public class DataSourceConfig {
    * Default DataSource.
    */
   @Bean
-  @SuppressFBWarnings(value = "NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE", justification = "Autowired")
   public DriverManagerDataSource dataSource() {
     DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-    driverManagerDataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-    driverManagerDataSource.setUrl(env.getProperty("jdbc.url"));
-    driverManagerDataSource.setUsername(env.getProperty("jdbc.username"));
-    driverManagerDataSource.setPassword(env.getProperty("jdbc.password"));
+    driverManagerDataSource.setDriverClassName(getProperty("jdbc.driverClassName"));
+    driverManagerDataSource.setUrl(getProperty("jdbc.url"));
+    driverManagerDataSource.setUsername(getProperty("jdbc.username"));
+    driverManagerDataSource.setPassword(getProperty("jdbc.password"));
     return driverManagerDataSource;
   }
 
@@ -44,38 +42,44 @@ public class DataSourceConfig {
    * Default entityManagerFactory.
    */
   @Bean
-  @SuppressFBWarnings(value = "NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE", justification = "Autowired")
   public LocalContainerEntityManagerFactoryBean entityManagerFactory(
       DriverManagerDataSource dataSource) {
     LocalContainerEntityManagerFactoryBean entityManagerFactory =
         new LocalContainerEntityManagerFactoryBean();
     entityManagerFactory.setDataSource(dataSource);
     Properties jpaProperties = new Properties();
-    jpaProperties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
-    jpaProperties.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-    jpaProperties.setProperty("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
+    jpaProperties.setProperty("hibernate.dialect", getProperty("hibernate.dialect"));
+    jpaProperties.setProperty("hibernate.show_sql", getProperty("hibernate.show_sql"));
+    jpaProperties.setProperty("hibernate.format_sql", getProperty("hibernate.format_sql"));
     jpaProperties.setProperty(
-        "hibernate.generate_statistics", env.getProperty("hibernate.generate_statistics"));
-    jpaProperties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
-    jpaProperties.setProperty(
-        "connection.provider_class", env.getProperty("connection.provider_class"));
+        "hibernate.generate_statistics", getProperty("hibernate.generate_statistics"));
+    jpaProperties.setProperty("hibernate.hbm2ddl.auto", getProperty("hibernate.hbm2ddl.auto"));
+    jpaProperties.setProperty("connection.provider_class",
+        getProperty("connection.provider_class"));
     jpaProperties.setProperty("hibernate.c3p0.acquire_increment",
-        env.getProperty("hibernate.c3p0.acquire_increment"));
+        getProperty("hibernate.c3p0.acquire_increment"));
     jpaProperties.setProperty(
-        "hibernate.c3p0.idle_test_period", env.getProperty("hibernate.c3p0.idle_test_period"));
+        "hibernate.c3p0.idle_test_period", getProperty("hibernate.c3p0.idle_test_period"));
     jpaProperties.setProperty(
-        "hibernate.c3p0.min_size", env.getProperty("hibernate.c3p0.min_size"));
+        "hibernate.c3p0.min_size", getProperty("hibernate.c3p0.min_size"));
     jpaProperties.setProperty(
-        "hibernate.c3p0.max_size", env.getProperty("hibernate.c3p0.max_size"));
+        "hibernate.c3p0.max_size", getProperty("hibernate.c3p0.max_size"));
     jpaProperties.setProperty(
-        "hibernate.c3p0.max_statements", env.getProperty("hibernate.c3p0.max_statements"));
-    jpaProperties.setProperty("hibernate.c3p0.timeout", env.getProperty("hibernate.c3p0.timeout"));
+        "hibernate.c3p0.max_statements", getProperty("hibernate.c3p0.max_statements"));
+    jpaProperties.setProperty("hibernate.c3p0.timeout", getProperty("hibernate.c3p0.timeout"));
     jpaProperties.setProperty("hibernate.c3p0.acquireRetryAttempts",
-        env.getProperty("hibernate.c3p0.acquireRetryAttempts"));
+        getProperty("hibernate.c3p0.acquireRetryAttempts"));
     jpaProperties.setProperty("hibernate.c3p0.acquireRetryDelay",
-        env.getProperty("hibernate.c3p0.acquireRetryDelay"));
+        getProperty("hibernate.c3p0.acquireRetryDelay"));
     entityManagerFactory.setJpaProperties(jpaProperties);
     return entityManagerFactory;
+  }
+
+  /**
+   * Get property from the environment.
+   */
+  private String getProperty(String property) {
+    return env.getProperty(property, "");
   }
 
   /**
