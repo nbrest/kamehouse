9e234fc74c4c8d4cc7c8cd972c446e01f7e9be62
nbrest <brest.nico@gmail.com>
2021-09-06 19:19:51 +1000

    Added test scheduler integration tests

---
+++ kamehouse-testmodule/src/test/java/com/nicobrest/kamehouse/testmodule/integration/TestSchedulerControllerIntegrationTest.java
@@ -0,0 +1,49 @@
+package com.nicobrest.kamehouse.testmodule.integration;
+
+import com.nicobrest.kamehouse.commons.integration.AbstractControllerIntegrationTest;
+import com.nicobrest.kamehouse.commons.model.KameHouseGenericResponse;
+import org.apache.http.HttpResponse;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Integration tests for the TestSchedulerController class.
+ *
+ * @author nbrest
+ */
+public class TestSchedulerControllerIntegrationTest extends AbstractControllerIntegrationTest {
+
+  private static final String API_URL = "/api/v1/test-module/test-scheduler/sample-job";
+
+  @Override
+  public String getWebapp() {
+    return "kame-house-testmodule";
+  }
+
+  @Test
+  public void sampleJobScheduleTest() throws Exception {
+    logger.info("Running sampleJobScheduleTest");
+
+    HttpResponse response = post(getWebappUrl() + API_URL + "?delay=2");
+
+    verifySuccessfulCreatedResponse(response, KameHouseGenericResponse.class);
+  }
+
+  @Test
+  public void sampleJobStatusTest() throws Exception {
+    logger.info("Running sampleJobStatusTest");
+
+    HttpResponse response = get(getWebappUrl() + API_URL);
+
+    verifySuccessfulResponse(response, KameHouseGenericResponse.class);
+  }
+
+  @Test
+  public void sampleJobCancelTest() throws Exception {
+    logger.info("Running sampleJobCancelTest");
+
+    HttpResponse response = delete(getWebappUrl() + API_URL);
+
+    verifySuccessfulResponse(response, KameHouseGenericResponse.class);
+  }
+}
+
