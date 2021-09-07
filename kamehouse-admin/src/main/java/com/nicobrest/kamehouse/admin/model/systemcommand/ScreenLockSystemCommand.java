5b920750664c8b3c99b55bd81e80c50538241711
nbrest <brest.nico@gmail.com>
2021-09-08 01:56:48 +1000

    Fix screen lock on linux ci

--- kamehouse-admin/src/main/java/com/nicobrest/kamehouse/admin/model/systemcommand/ScreenLockSystemCommand.java
+++ kamehouse-admin/src/main/java/com/nicobrest/kamehouse/admin/model/systemcommand/ScreenLockSystemCommand.java
@@ -12,7 +12,7 @@ public class ScreenLockSystemCommand extends SystemCommand {
 
   /** Sets the command line for each operation system required for this SystemCommand. */
   public ScreenLockSystemCommand() {
-    linuxCommand.addAll(Arrays.asList("/bin/bash", "-c", "gnome-screensaver-command -l"));
+    linuxCommand.addAll(Arrays.asList("/bin/bash", "-c", "DISPLAY=:0.0 gnome-screensaver-command -l"));
     windowsCommand.addAll(
         Arrays.asList("cmd.exe", "/c", "start", "rundll32.exe", "user32.dll,LockWorkStation"));
     setOutputCommand();
