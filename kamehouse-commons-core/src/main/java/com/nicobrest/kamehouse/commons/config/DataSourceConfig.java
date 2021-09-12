bb30441c09d26857db0cdb67a1fd0450ea70b2c6
nbrest <brest.nico@gmail.com>
2021-09-12 14:08:15 +1000

    Removed unused import

--- kamehouse-commons-core/src/main/java/com/nicobrest/kamehouse/commons/config/DataSourceConfig.java
+++ kamehouse-commons-core/src/main/java/com/nicobrest/kamehouse/commons/config/DataSourceConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
