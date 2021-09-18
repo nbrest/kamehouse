629cc384588b9506ad44421381d81221a1ef3c65
nbrest <brest.nico@gmail.com>
2021-09-18 16:40:23 +1000

    Updated build script

--- docker/build.sh
+++ docker/build.sh
@@ -5,4 +5,4 @@ docker build -t java.web.kamehouse .
 
 echo "Recreating docker container java.web.kamehouse"
 docker container rm java.web.kamehouse
-docker create --name java.web.kamehouse -p 6022:22 -p 6080:80 -p 6443:443 -p 6306:3306 -p 6090:9090 java.web.kamehouse
+docker create --name java.web.kamehouse -p 6022:22 -p 6080:80 -p 6443:443 -p 6090:9090 java.web.kamehouse
