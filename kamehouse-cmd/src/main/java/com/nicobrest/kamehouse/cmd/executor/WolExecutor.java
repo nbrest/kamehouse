d2da6a3e0828186ca36f68404162390a2e985db5
nbrest <brest.nico@gmail.com>
2022-03-02 23:47:20 +1100

    Added logging

--- kamehouse-cmd/src/main/java/com/nicobrest/kamehouse/cmd/executor/WolExecutor.java
+++ kamehouse-cmd/src/main/java/com/nicobrest/kamehouse/cmd/executor/WolExecutor.java
@@ -2,6 +2,8 @@ package com.nicobrest.kamehouse.cmd.executor;
 
 import com.nicobrest.kamehouse.cmd.model.CmdArgumentHandler;
 import com.nicobrest.kamehouse.commons.utils.NetworkUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,12 +14,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class WolExecutor implements Executor {
 
+  private final Logger logger = LoggerFactory.getLogger(WolExecutor.class);
+
   /**
    * Execute the operation.
    */
   public void execute(CmdArgumentHandler cmdArgumentHandler) {
     String macAddress = cmdArgumentHandler.getArgument("mac");
     String broadcastAddress = cmdArgumentHandler.getArgument("broadcast");
+    logger.info("Sending wol packet to mac {} and broadcast {}", macAddress, broadcastAddress);
     NetworkUtils.wakeOnLan(macAddress, broadcastAddress);
   }
 }
