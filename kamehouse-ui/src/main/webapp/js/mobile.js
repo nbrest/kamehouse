ee83142eac5210ae882943b0f544216553043986
nbrest <brest.nico@gmail.com>
2022-03-09 18:45:04 +1100

    - Added disclaimers to kamehouse-mobile and tennisworld bookings - Added slideshow to mobile page - Updated android download button styles and icon

--- kamehouse-ui/src/main/webapp/js/mobile.js
+++ kamehouse-ui/src/main/webapp/js/mobile.js
@@ -1,8 +1,12 @@
 /**
- * Mobile app link page startup function. Generate QR code.
+ * Mobile app link page startup function.
  */
+ var kameHouseSlideShowManager;
+
 function mainMobileLinks() {
   generateAndroidQrCode();
+  kameHouseSlideShowManager = new KameHouseSlideShowManager();
+  kameHouseSlideShowManager.setDotSlide(1);
 }
 
 function generateAndroidQrCode() {
@@ -11,6 +15,68 @@ function generateAndroidQrCode() {
     ecLevel:'L',
     size: 256
   });
+}
+
+/**
+ * Functionality to update the slides view in a slideshow.
+ */
+function KameHouseSlideShowManager() {
+  let slideIndex = 1;
+
+  this.prevSlide = prevSlide;
+  this.nextSlide = nextSlide;
+  this.setDotSlide = setDotSlide;
+
+  /**
+   * Show previous slide.
+   */
+  function prevSlide() {
+    changeSlide(-1);
+  }
+
+  /**
+   * Show next slide.
+   */
+  function nextSlide() {
+    changeSlide(1);
+  }
+
+  /**
+   * Add or substract the specified value to the slide index and update the slide.
+   */
+  function changeSlide(indexValueToAdd) {
+    updateSlide(slideIndex += indexValueToAdd);
+  }
+  
+  /**
+   * Show the slide of the specified dot index.
+   */
+  function setDotSlide(dotNumber) {
+    slideIndex = dotNumber;
+    updateSlide(dotNumber);
+  }
+  
+  /**
+   * Update the slide view with the specified slide index.
+   */
+  function updateSlide(chosenSlideIndex) {
+    const slides = document.getElementsByClassName("kamehouse-slideshow-slide");
+    const dots = document.getElementsByClassName("kamehouse-slideshow-dot");
+    if (chosenSlideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    if (chosenSlideIndex < 1) {
+      slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+      domUtils.setDisplay(slides[i], "none");
+    }
+    for (let i = 0; i < dots.length; i++) {
+      domUtils.classListRemove(dots[i], "kamehouse-slideshow-dot-active")
+    }
+    domUtils.setDisplay(slides[slideIndex-1], "block");
+    domUtils.classListAdd(dots[slideIndex-1], "kamehouse-slideshow-dot-active");
+  }
 }
 
 $(document).ready(mainMobileLinks);
