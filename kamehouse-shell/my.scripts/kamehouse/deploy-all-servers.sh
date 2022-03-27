b82ac53a3a4893b53dd365ca2453adb808da1950
nbrest <brest.nico@gmail.com>
2022-03-27 18:08:04 +1100

    Exported my.scripts

--- kamehouse-shell/my.scripts/kamehouse/deploy-all-servers.sh
+++ kamehouse-shell/my.scripts/kamehouse/deploy-all-servers.sh
@@ -19,20 +19,23 @@ mainProcess() {
 
 deployInAllServers() {
   # niko-server
-  deployInServer "niko-server" "80" "win" &
-  #deployInServer "niko-server" "7080" "lin" &
+  deployInServer "niko-server" "80" "win" "false" &
+
   # niko-server-vm-ubuntu
-  deployInServer "niko-server-vm-ubuntu" "80" "lin" &
-  #deployInServer "niko-server-vm-ubuntu" "7080" "lin" &
+  deployInServer "niko-server-vm-ubuntu" "80" "lin" "false" &
+  deployInServer "niko-server-vm-ubuntu" "12080" "lin" "true" &
+
   # pi
-  deployInServer "pi" "80" "lin" &
-  #deployInServer "pi" "7080" "lin" &
+  deployInServer "pi" "80" "lin" "false" &
+  
   # niko-nba
-  deployInServer "niko-nba" "80" "win" &
+  deployInServer "niko-nba" "80" "win" "false" &
+
   # niko-w
-  deployInServer "niko-w" "80" "win" &
+  deployInServer "niko-w" "80" "win" "false" &
+
   # niko-w-vm-ubuntu
-  deployInServer "niko-w-vm-ubuntu" "80" "lin" &
+  deployInServer "niko-w-vm-ubuntu" "80" "lin" "false" &
 
   log.info "Waiting for deployment to finish in all servers. ${COL_YELLOW}This process can take several minutes"
   wait
@@ -43,9 +46,10 @@ deployInServer() {
   local SERVER=$1
   local PORT=$2
   local HOST_OS=$3
+  local IS_DOCKER_DEMO=$4
   log.info "Started deployInServer ${COL_PURPLE}${SERVER}:${PORT}:${HOST_OS}"
-  gitPullAll ${SERVER} ${PORT} ${HOST_OS} &
-  deployKamehouse ${SERVER} ${PORT} &
+  gitPullAll ${SERVER} ${PORT} ${HOST_OS} ${IS_DOCKER_DEMO} &
+  deployKamehouse ${SERVER} ${PORT} ${IS_DOCKER_DEMO} &
   wait
   log.info "${COL_RED}Finished deployInServer ${COL_CYAN}${SERVER}:${PORT}:${HOST_OS}"
 }
@@ -54,36 +58,47 @@ gitPullAll() {
   local SERVER=$1
   local PORT=$2
   local HOST_OS=$3
+  local IS_DOCKER_DEMO=$4
   log.info "Started gitPullAll ${COL_PURPLE}${SERVER}:${PORT}:${HOST_OS}"
-  executeScriptInServer ${SERVER} ${PORT} "${HOST_OS}/git/git-pull-all.sh"
+  executeScriptInServer ${SERVER} ${PORT} "${HOST_OS}/git/git-pull-all.sh" ${IS_DOCKER_DEMO}
   log.info "Finished gitPullAll ${COL_PURPLE}${SERVER}:${PORT}:${HOST_OS}"
 }
 
 deployKamehouse() {
   local SERVER=$1
   local PORT=$2
+  local IS_DOCKER_DEMO=$3
   local SCRIPT_ARGS="-f"
   if [ -n "${MODULE_SHORT}" ]; then
     SCRIPT_ARGS="${SCRIPT_ARGS} -m ${MODULE_SHORT}"
   fi
   log.info "Started deployKamehouse ${COL_PURPLE}${SERVER}:${PORT}"
-  executeScriptInServer ${SERVER} ${PORT} "kamehouse/deploy-java-web-kamehouse.sh" "${SCRIPT_ARGS}"
+  executeScriptInServer ${SERVER} ${PORT} ${IS_DOCKER_DEMO} "kamehouse/deploy-java-web-kamehouse.sh" "${SCRIPT_ARGS}"
   log.info "Finished deployKamehouse ${COL_PURPLE}${SERVER}:${PORT}"
 }
 
 executeScriptInServer() {
   local SERVER=$1
   local PORT=$2
-  local SCRIPT=$3
-  local SCRIPT_ARGS="$4"
-  local URL=
-  local URL_ENCODED_PARAMS=
-  
+  local IS_DOCKER_DEMO=$3
+  local SCRIPT=$4
+  local SCRIPT_ARGS="$5"
+  local URL=""
+  local URL_ENCODED_PARAMS=""
+  local BASIC_AUTH=""
+
   if [ -n "${SCRIPT_ARGS}" ]; then 
     URL_ENCODED_PARAMS="script=${SCRIPT}&args="$(urlencode "${SCRIPT_ARGS}")
   else
     URL_ENCODED_PARAMS="script=${SCRIPT}"
   fi
+
+  if ${IS_DOCKER_DEMO}; then
+    BASIC_AUTH=${DOCKER_DEMO_GROOT_API_BASIC_AUTH}
+  else
+    BASIC_AUTH=${GROOT_API_BASIC_AUTH}
+  fi
+
   URL="http://${SERVER}:${PORT}/kame-house-groot/api/v1/admin/my-scripts/exec-script.php?${URL_ENCODED_PARAMS}"
   log.debug "Executing request: ${COL_BLUE}${URL}"
   RESPONSE=`curl --max-time 1800 -k --location --request GET "${URL}" --header "Authorization: Basic ${GROOT_API_BASIC_AUTH}" 2>/dev/null`
