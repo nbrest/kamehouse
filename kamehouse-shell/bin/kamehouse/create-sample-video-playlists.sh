5418f1d0ff54901e4b116016587e6b763175ed13
nbrest <brest.nico@gmail.com>
2022-03-30 23:17:24 +1100

    Updated docker cache

--- kamehouse-shell/bin/kamehouse/create-sample-video-playlists.sh
+++ kamehouse-shell/bin/kamehouse/create-sample-video-playlists.sh
@@ -10,15 +10,15 @@ fi
 LOG_PROCESS_TO_FILE=true
 
 mainProcess() {
-  log.info "Create fake video playlists"
+  log.info "Create sample video playlists"
   if [ -d "${HOME}/git/kamehouse-video-playlists/.git" ]; then
-    log.warn "${HOME}/git/kamehouse-video-playlists is a git repository. No need to fake playlists. Exiting..."
+    log.warn "${HOME}/git/kamehouse-video-playlists is a git repository. No need to create sample playlists. Exiting..."
     exit 1
   fi
   mkdir -p ${HOME}/git/kamehouse-video-playlists/playlists/http-media-server-ip/media-drive/
   cp -vr ${HOME}/git/kamehouse/docker/media/playlist/* ${HOME}/git/kamehouse-video-playlists/playlists/http-media-server-ip/media-drive/
   if [ ! -d "${HOME}/docker" ]; then
-    log.info "Creating ${HOME}/docker directory"
+    log.info "Creating ${HOME}/docker directory to load videos from there"
     cp -r ${HOME}/git/kamehouse/docker ${HOME}/docker
   fi
 }
