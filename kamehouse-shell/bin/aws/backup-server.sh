06f6cad1e079e8b7b06fa89eec5fd1483e56f930
nbrest <brest.nico@gmail.com>
2022-04-03 01:59:09 +1100

    Fixed backup server ssh to docker

--- kamehouse-shell/bin/aws/backup-server.sh
+++ kamehouse-shell/bin/aws/backup-server.sh
@@ -6,7 +6,12 @@ if [ "$?" != "0" ]; then
   echo "$(date +%Y-%m-%d' '%H:%M:%S) - [ERROR] - An error occurred importing common-functions.sh"
   exit 1
 fi
-
+# Import kamehouse functions
+source ${HOME}/programs/kamehouse-shell/bin/common/kamehouse/kamehouse-functions.sh
+if [ "$?" != "0" ]; then
+  echo -e "\033[1;36m$(date +%Y-%m-%d' '%H:%M:%S)\033[0;39m - [\033[1;31mERROR\033[0;39m] - \033[1;31mAn error occurred importing kamehouse-functions.sh\033[0;39m"
+  exit 1
+fi
 source ${HOME}/programs/kamehouse-shell/bin/common/backup/backup-server-functions.sh
 if [ "$?" != "0" ]; then
 	echo "$(date +%Y-%m-%d' '%H:%M:%S) - [ERROR] - An error occurred importing backup-server-functions.sh"
