8f62a0963038fa76cfa200a62852dc15845ef1f2
nbrest <brest.nico@gmail.com>
2022-04-02 12:39:26 +1100

    Updated alias

--- kamehouse-shell/bin/common/bashrc/alias.sh
+++ kamehouse-shell/bin/common/bashrc/alias.sh
@@ -1,12 +1,12 @@
 # common aliases
-alias cd.eclipse.kamehouse='cd ${HOME}/workspace-eclipse/kamehouse'
+alias cd.eclipse='cd ${HOME}/workspace-eclipse/kamehouse'
 alias cd.git="cd ${HOME}/git"
-alias cd.kamehouse='cd ${HOME}/workspace-intellij/kamehouse'
+alias cd.kamehouse='cd ${HOME}/git/kamehouse'
 alias cd.logs='cd ${HOME}/logs'
 alias cd.my.scripts="cd ${HOME}/my.scripts"
 alias cd.texts='cd ${HOME}/git/texts'
 alias cd.shell="cd ${HOME}/programs/kamehouse-shell/bin"
-alias cd.intellij='cd ${HOME}/workspace-intellij'
+alias cd.intellij='cd ${HOME}/workspace-intellij/kamehouse'
 
 alias ksi="kamehouse-shell-install.sh"
 
