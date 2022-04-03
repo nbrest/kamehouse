df48e417892efde16dcfd86ab27272e7b30050c1
nbrest <brest.nico@gmail.com>
2022-04-03 13:06:52 +1000

    Added log filtering based on log level to shell scripts, with default log level INFO

--- kamehouse-shell/bin/common/log-functions.sh
+++ kamehouse-shell/bin/common/log-functions.sh
@@ -3,6 +3,15 @@
 #######################################################################
 # Log functions to log events to the console with a more robust framework than just echoing.
 
+# Default log level is INFO. Log everything included in this level and under.
+# Modify this by setting LOG_LEVEL=DEBUG scrit-name.sh while running the script
+# 0: ERROR
+# 1: WARN
+# 2: INFO
+# 3: DEBUG
+# 4: TRACE
+LOG_LEVEL_NUMBER=2
+
 # Default color used in logs. Use this variable to return to default color if
 # I change the color of some words in the log entry.
 COL_DEFAULT_LOG=${COL_GREEN}
@@ -13,6 +22,13 @@ log() {
   local LEVEL=$1
   # convert log level to upper case
   LEVEL=`echo "${LEVEL}" | tr '[:lower:]' '[:upper:]'`
+
+  local CURRENT_ENTRY_LOG_LEVEL=`getLogLevelNumber ${LEVEL}`
+  if (( ${CURRENT_ENTRY_LOG_LEVEL} > ${LOG_LEVEL_NUMBER} )); then
+    # Skip log entry. log level is set below current entry
+    return
+  fi
+
   local MESSAGE=$2
   # convert \n to literal '\n' in message string so it doesnt take \n as new line. For example in C:\Users\nicolas.brest
   MESSAGE=${MESSAGE//\\\n/\\\\n}
@@ -80,3 +96,44 @@ logStart() {
 logFinish() {
   log.info "Finished executing ${COL_PURPLE}${SCRIPT_NAME}${COL_DEFAULT_LOG} with command line arguments ${COL_PURPLE}\"${CMD_ARGUMENTS}\"${COL_DEFAULT_LOG} successfully."
 }
+
+# Get the current log level number
+getLogLevelNumber() {
+  local LEVEL=$1
+  LEVEL=`echo "${LEVEL}" | tr '[:lower:]' '[:upper:]'`
+
+  if [ "${LEVEL}" == "TRACE" ]; then
+    echo "4"
+    return
+  fi
+
+  if [ "${LEVEL}" == "DEBUG" ]; then
+    echo "3"
+    return
+  fi
+
+  if [ "${LEVEL}" == "INFO" ]; then
+    echo "2"
+    return
+  fi
+
+  if [ "${LEVEL}" == "WARN" ]; then
+    echo "1"
+    return
+  fi
+
+  if [ "${LEVEL}" == "ERROR" ]; then
+    echo "0"
+    return
+  fi
+
+  echo "2"
+}
+
+# Set global log level from environment
+setGlobalLogLevelFromEnv() {
+  if [ -n "${LOG_LEVEL}" ]; then
+    LOG_LEVEL_NUMBER=`getLogLevelNumber ${LOG_LEVEL}`
+  fi
+}
+setGlobalLogLevelFromEnv
