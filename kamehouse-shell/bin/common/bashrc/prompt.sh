33f08d164cc8b8d4618d83137c2f9a9390b97881
nbrest <brest.nico@gmail.com>
2022-06-22 17:53:09 +1000

    Updated kamehouse-shell prompt

--- kamehouse-shell/bin/common/bashrc/prompt.sh
+++ kamehouse-shell/bin/common/bashrc/prompt.sh
@@ -6,30 +6,30 @@ if (( $EUID != 0 )); then
   # Normal user
   PS1_PREFIX="\[\e]0;\u@\h:\w\a\]"
   PS1_SCREEN="\[${COL_BLUE}\]S:"
+  PS1_KAMEHOUSE="\[${COL_BLUE}\][\[${COL_PURPLE}\]KameHouse\[${COL_BLUE}\]] "
   PS1_USERNAME="\[${COL_GREEN}\]\u"
   PS1_AT="\[${COL_BLUE}\]@"
   PS1_HOSTNAME="\[${COL_PURPLE}\]\h"
-  PS1_KAMEHOUSE="\[${COL_BLUE}\][\[${COL_PURPLE}\]KameHouse\[${COL_BLUE}\]]"
   PS1_DOT="\[${COL_BLUE}\]:"
   PS1_PWD="\[${COL_CYAN}\]\w"
-  PS1_GIT="\[${COL_YELLOW}\]"'$(__git_ps1 " (%s)")'
+  PS1_GIT="\[${COL_YELLOW}\]"'`__git_ps1 " (%s)"`'
   PS1_SUFFIX="\[${COL_BLUE}\]\$ \[${COL_NORMAL}\]"
 else
   # root
   PS1_PREFIX="\[\e]0;\u@\h:\w\a\]"
   PS1_SCREEN="\[${COL_BLUE}\]S:"
+  PS1_KAMEHOUSE="\[${COL_BLUE}\][\[${COL_RED}\]KameHouse\[${COL_BLUE}\]] "
   PS1_USERNAME="\[${COL_RED}\]\u"
   PS1_AT="\[${COL_BLUE}\]@"
   PS1_HOSTNAME="\[${COL_RED}\]\h"
-  PS1_KAMEHOUSE="\[${COL_BLUE}\][\[${COL_RED}\]KameHouse\[${COL_BLUE}\]]"
   PS1_DOT="\[${COL_BLUE}\]:"
   PS1_PWD="\[${COL_CYAN}\]\w"
-  PS1_GIT="\[${COL_YELLOW}\]"'$(__git_ps1 " (%s)")'
+  PS1_GIT="\[${COL_YELLOW}\]"'`__git_ps1 " (%s)"`'
   PS1_SUFFIX="\[${COL_BLUE}\]\$ \[${COL_NORMAL}\]"
 fi
 
 if [ -z "$STY" ]; then
-  PS1=${PS1_PREFIX}${PS1_USERNAME}${PS1_AT}${PS1_HOSTNAME}${PS1_KAMEHOUSE}${PS1_DOT}${PS1_PWD}${PS1_GIT}${PS1_SUFFIX}  
+  PS1=${PS1_PREFIX}${PS1_KAMEHOUSE}${PS1_USERNAME}${PS1_AT}${PS1_HOSTNAME}${PS1_DOT}${PS1_PWD}${PS1_GIT}${PS1_SUFFIX}  
 else
-  PS1=${PS1_PREFIX}${PS1_SCREEN}${PS1_USERNAME}${PS1_AT}${PS1_HOSTNAME}${PS1_KAMEHOUSE}${PS1_DOT}${PS1_PWD}${PS1_GIT}${PS1_SUFFIX}
+  PS1=${PS1_PREFIX}${PS1_SCREEN}${PS1_KAMEHOUSE}${PS1_USERNAME}${PS1_AT}${PS1_HOSTNAME}${PS1_DOT}${PS1_PWD}${PS1_GIT}${PS1_SUFFIX}
 fi
