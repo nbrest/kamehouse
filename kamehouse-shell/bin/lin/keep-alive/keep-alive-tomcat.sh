78c6079a8aa621cc4704bf3adbfe207f9fe02a2b
nbrest <brest.nico@gmail.com>
2022-06-27 18:44:10 +1000

    Removed dependency on sudo

--- kamehouse-shell/bin/lin/keep-alive/keep-alive-tomcat.sh
+++ kamehouse-shell/bin/lin/keep-alive/keep-alive-tomcat.sh
@@ -14,7 +14,7 @@ SERVICE="tomcat"
 SERVICE_STARTUP="${HOME}/programs/kamehouse-shell/bin/kamehouse/tomcat-restart.sh"
 
 mainProcess() {
-  PID=`sudo netstat -nltp | grep ${PORT} | grep java | awk '{print $7}' | cut -d '/' -f 1`
+  PID=`netstat -nltp | grep ${PORT} | grep java | awk '{print $7}' | cut -d '/' -f 1`
   if [ -z ${PID} ]; then
     log.info "${SERVICE} not running. Starting it now"
     ${SERVICE_STARTUP} &
