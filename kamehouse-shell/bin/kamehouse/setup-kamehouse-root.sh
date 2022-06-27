eca552d166aefea7f18f0f28a3f7fdebf8d3b682
nbrest <brest.nico@gmail.com>
2022-06-27 22:21:55 +1000

    Updated dockerfile and root setup script

--- kamehouse-shell/bin/kamehouse/setup-kamehouse-root.sh
+++ kamehouse-shell/bin/kamehouse/setup-kamehouse-root.sh
@@ -17,43 +17,9 @@ COL_MESSAGE=${COL_GREEN}
 main() {
   parseArguments "$@"
   log.info "Setting up root user for kamehouse"
-  log.warn "User running this script needs ${COL_RED}sudo bash,ln,mkdir${COL_DEFAULT_LOG} permissions"
-  createLogsDir
-  createRootSymLink
-  FUNC=$(declare -f updateRootBashRc)
-  sudo bash -c "$FUNC; updateRootBashRc"
+  log.warn "User running this script needs ${COL_RED}sudo su${COL_DEFAULT_LOG} permissions"
+  sudo su -c "cd /home/${KAMEHOUSE_USERNAME}/git/kamehouse ; ./kamehouse-shell/bin/kamehouse/install-kamehouse-shell.sh"
   log.info "Finished setting up root user for kamehouse"
-}
-
-createLogsDir() {
-  sudo mkdir -p /root/logs
-}
-
-createRootSymLink() {
-  local USERNAME=`whoami`
-  log.info "Creating symlink on root home" 
-  sudo ln -s /home/${USERNAME}/programs /root/
-  log.info "Ignore ${COL_PURPLE}sudo${COL_MESSAGE} error on windows, give ${COL_PURPLE}sudo${COL_MESSAGE} permissions to current user on linux if it fails on linux"
-}
-
-# Call this function with sudo to execute as root
-updateRootBashRc() {
-  INFO=" - [INFO] - "
-  echo -e "$(date +%Y-%m-%d' '%H:%M:%S)${INFO}Updating /root/.bashrc"
-  if [ ! -f "/root/.bashrc" ]; then
-    echo -e "$(date +%Y-%m-%d' '%H:%M:%S)${INFO}/root/.bashrc not found. Creating one"
-    echo "" > /root/.bashrc
-    echo "source \${HOME}/programs/kamehouse-shell/bin/common/bashrc/bashrc.sh" >> /root/.bashrc
-  else
-    cat /root/.bashrc | grep "/programs/kamehouse-shell/bin/common/bashrc/bashrc.sh" > /dev/null
-    if [ "$?" != "0" ]; then
-      echo -e "$(date +%Y-%m-%d' '%H:%M:%S)${INFO}Adding bashrc/bashrc.sh to /root/.bashrc"
-      echo "" >> /root/.bashrc
-      echo "source \${HOME}/programs/kamehouse-shell/bin/common/bashrc/bashrc.sh" >> /root/.bashrc
-    else
-      echo -e "$(date +%Y-%m-%d' '%H:%M:%S)${INFO}/root/.bashrc already sources \${HOME}/programs/kamehouse-shell/bin/common/bashrc/bashrc.sh. No need to update"
-    fi
-  fi
 }
 
 log.info() {
