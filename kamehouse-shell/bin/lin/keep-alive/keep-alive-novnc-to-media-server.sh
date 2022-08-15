a7eb37cbc4af48ea5044cad862f8a9f67be14c20
nbrest <brest.nico@gmail.com>
2022-08-15 20:43:03 +1000

    Updated keep alive novnc

--- kamehouse-shell/bin/lin/keep-alive/keep-alive-novnc-to-media-server.sh
+++ kamehouse-shell/bin/lin/keep-alive/keep-alive-novnc-to-media-server.sh
@@ -8,25 +8,12 @@ if [ "$?" != "0" ]; then
 fi
 
 LOG_PROCESS_TO_FILE=false
-PRIVATE_KEY=/etc/letsencrypt/live/www.nicobrest.com/privkey.pem
-CERT=/etc/letsencrypt/live/www.nicobrest.com/fullchain.pem
-NOVNC_KEYS_DIR=${HOME}/git/noVNC
 MEDIA_SERVER_IP=192.168.0.109
 
 mainProcess() {
   log.info "Call this script with nohup"
-  #copyCerts
   cd ${HOME}/git/noVNC
-  #./utils/novnc_proxy --vnc ${MEDIA_SERVER_IP}:5900 --listen 3900 --cert ${NOVNC_KEYS_DIR}/fullchain.pem --key ${NOVNC_KEYS_DIR}/privkey.pem > ${HOME}/logs/novnc.log 2>&1 &
   ./utils/novnc_proxy --vnc ${MEDIA_SERVER_IP}:5900 --listen 3900 > ${HOME}/logs/novnc.log 2>&1 &
-}
-
-copyCerts() {
-  mkdir -p ${NOVNC_KEYS_DIR}
-  sudo cp -L ${PRIVATE_KEY} ${NOVNC_KEYS_DIR}/fullchain.pem
-  sudo chmod a+xr ${NOVNC_KEYS_DIR}/fullchain.pem
-  sudo cp -L ${CERT} ${NOVNC_KEYS_DIR}/privkey.pem
-  sudo chmod a+xr ${NOVNC_KEYS_DIR}/privkey.pem
 }
 
 main "$@"
