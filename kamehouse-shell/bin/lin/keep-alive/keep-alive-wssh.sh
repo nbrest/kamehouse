426e400689e9c96a1c7e37ed2c892db7b36471c5
nbrest <brest.nico@gmail.com>
2022-08-15 11:37:25 +1000

    Added keep alive wssh

---
+++ kamehouse-shell/bin/lin/keep-alive/keep-alive-wssh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Import common functions
+source ${HOME}/programs/kamehouse-shell/bin/common/common-functions.sh
+if [ "$?" != "0" ]; then
+  echo -e "\033[1;36m$(date +%Y-%m-%d' '%H:%M:%S)\033[0;39m - [\033[1;31mERROR\033[0;39m] - \033[1;31mAn error occurred importing common-functions.sh\033[0;39m"
+  exit 1
+fi
+
+LOG_PROCESS_TO_FILE=false
+SERVICE="wssh"
+
+mainProcess() {
+  log.info "Call this script with nohup"
+  ${HOME}/.local/bin/wssh --port 3901 > ${HOME}/logs/wssh.log 2>&1 &
+}
+
+main "$@"
