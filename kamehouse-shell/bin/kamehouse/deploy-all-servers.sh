d5fb1b5af04442d5b30a5efac3db9a6fee00a00f
nbrest <brest.nico@gmail.com>
2022-08-26 21:29:34 +1000

    Fixed deploy all servers script

--- kamehouse-shell/bin/kamehouse/deploy-all-servers.sh
+++ kamehouse-shell/bin/kamehouse/deploy-all-servers.sh
@@ -24,23 +24,23 @@ mainProcess() {
 
 deployInAllServers() {
   # niko-server
-  deployInServer "niko-server" "80" "win" "false" &
+  deployInServer "niko-server" "80" "win" "false" "false" &
 
   # niko-server-vm-ubuntu
-  deployInServer "niko-server-vm-ubuntu" "80" "lin" "false" &
-  deployInServer "niko-server-vm-ubuntu" "12080" "lin" "true" &
+  deployInServer "niko-server-vm-ubuntu" "80" "lin" "false" "false" &
+  deployInServer "niko-server-vm-ubuntu" "12080" "lin" "true" "false" &
 
   # pi
-  deployInServer "pi" "80" "lin" "false" &
+  deployInServer "pi" "80" "lin" "false" "true" &
   
   # niko-nba
-  deployInServer "niko-nba" "80" "win" "false" &
+  deployInServer "niko-nba" "80" "win" "false" "false" &
 
   # niko-w
-  deployInServer "niko-w" "80" "win" "false" &
+  deployInServer "niko-w" "80" "win" "false" "false" &
 
   # niko-w-vm-ubuntu
-  deployInServer "niko-w-vm-ubuntu" "80" "lin" "false" &
+  deployInServer "niko-w-vm-ubuntu" "80" "lin" "false" "false" &
 
   log.info "Waiting for deployment to finish in all servers. ${COL_YELLOW}This process can take several minutes"
   wait
@@ -52,9 +52,10 @@ deployInServer() {
   local PORT=$2
   local HOST_OS=$3
   local IS_DOCKER_DEMO=$4
+  local IS_HTTPS=$5
   log.info "Started deployInServer ${COL_PURPLE}${SERVER}:${PORT}:${HOST_OS}"
-  gitPullAll ${SERVER} ${PORT} ${HOST_OS} ${IS_DOCKER_DEMO} &
-  deployKamehouse ${SERVER} ${PORT} ${IS_DOCKER_DEMO} &
+  gitPullAll ${SERVER} ${PORT} ${HOST_OS} ${IS_DOCKER_DEMO} ${IS_HTTPS} &
+  deployKamehouse ${SERVER} ${PORT} ${IS_DOCKER_DEMO} ${IS_HTTPS} &
   wait
   log.info "${COL_RED}Finished deployInServer ${COL_CYAN}${SERVER}:${PORT}:${HOST_OS}"
 }
@@ -64,8 +65,9 @@ gitPullAll() {
   local PORT=$2
   local HOST_OS=$3
   local IS_DOCKER_DEMO=$4
+  local IS_HTTPS=$5
   log.info "Started gitPullAll ${COL_PURPLE}${SERVER}:${PORT}:${HOST_OS}"
-  executeScriptInServer ${SERVER} ${PORT} ${IS_DOCKER_DEMO} "${HOST_OS}/git/git-pull-all.sh" 
+  executeScriptInServer ${SERVER} ${PORT} ${IS_DOCKER_DEMO} "${HOST_OS}/git/git-pull-all.sh" ${IS_HTTPS}
   log.info "Finished gitPullAll ${COL_PURPLE}${SERVER}:${PORT}:${HOST_OS}"
 }
 
@@ -73,12 +75,13 @@ deployKamehouse() {
   local SERVER=$1
   local PORT=$2
   local IS_DOCKER_DEMO=$3
+  local IS_HTTPS=$4
   local SCRIPT_ARGS=""
   if [ -n "${MODULE_SHORT}" ]; then
     SCRIPT_ARGS="${SCRIPT_ARGS} -m ${MODULE_SHORT}"
   fi
   log.info "Started deployKamehouse ${COL_PURPLE}${SERVER}:${PORT}"
-  executeScriptInServer ${SERVER} ${PORT} ${IS_DOCKER_DEMO} "kamehouse/deploy-kamehouse.sh" "${SCRIPT_ARGS}"
+  executeScriptInServer ${SERVER} ${PORT} ${IS_DOCKER_DEMO} "kamehouse/deploy-kamehouse.sh" "${SCRIPT_ARGS}" ${IS_HTTPS}
   log.info "Finished deployKamehouse ${COL_PURPLE}${SERVER}:${PORT}"
 }
 
@@ -88,9 +91,15 @@ executeScriptInServer() {
   local IS_DOCKER_DEMO=$3
   local SCRIPT=$4
   local SCRIPT_ARGS="$5"
+  local IS_HTTPS=$6
   local URL=""
   local URL_ENCODED_PARAMS=""
   local BASIC_AUTH=""
+  local PROTOCOL="http"
+
+  if ${IS_HTTPS}; then
+    PROTOCOL="https"
+  fi
 
   if [ -n "${SCRIPT_ARGS}" ]; then 
     URL_ENCODED_PARAMS="script=${SCRIPT}&args="$(urlencode "${SCRIPT_ARGS}")
@@ -104,7 +113,7 @@ executeScriptInServer() {
     BASIC_AUTH=${GROOT_API_BASIC_AUTH}
   fi
 
-  URL="http://${SERVER}:${PORT}/kame-house-groot/api/v1/admin/kamehouse-shell/exec-script.php?${URL_ENCODED_PARAMS}"
+  URL="${PROTOCOL}://${SERVER}:${PORT}/kame-house-groot/api/v1/admin/kamehouse-shell/exec-script.php?${URL_ENCODED_PARAMS}"
   log.info "Executing request: ${COL_BLUE}${URL}"
   RESPONSE=`curl --max-time 1800 -k --location --request GET "${URL}" --header "Authorization: Basic ${BASIC_AUTH}" 2>/dev/null`
   log.trace "${RESPONSE}"
--- kamehouse-shell/bin/kamehouse/git-pull-all-all-servers.sh
+++ kamehouse-shell/bin/kamehouse/git-pull-all-all-servers.sh
@@ -17,23 +17,23 @@ mainProcess() {
 
 gitPullAllAllServers() {
   # niko-server
-  gitPullAll "niko-server" "80" "win" "false" &
+  gitPullAll "niko-server" "80" "win" "false" "false" &
 
   # niko-server-vm-ubuntu
-  gitPullAll "niko-server-vm-ubuntu" "80" "lin" "false" &
-  gitPullAll "niko-server-vm-ubuntu" "12080" "lin" "true" &
+  gitPullAll "niko-server-vm-ubuntu" "80" "lin" "false" "false" &
+  gitPullAll "niko-server-vm-ubuntu" "12080" "lin" "true" "false" &
 
   # pi
-  gitPullAll "pi" "80" "lin" "false" &
+  gitPullAll "pi" "80" "lin" "false" "true" &
 
   # niko-nba
-  gitPullAll "niko-nba" "80" "win" "false" &
+  gitPullAll "niko-nba" "80" "win" "false" "false" &
 
   # niko-w
-  gitPullAll "niko-w" "80" "win" "false" &
+  gitPullAll "niko-w" "80" "win" "false" "false" &
 
   # niko-w-vm-ubuntu
-  gitPullAll "niko-w-vm-ubuntu" "80" "lin" "false" &
+  gitPullAll "niko-w-vm-ubuntu" "80" "lin" "false" "false" &
 
   log.info "Waiting for git pull all to finish in all servers. ${COL_YELLOW}This process can take several minutes"
   wait
@@ -45,8 +45,9 @@ gitPullAll() {
   local PORT=$2
   local HOST_OS=$3
   local IS_DOCKER_DEMO=$4
+  local IS_HTTPS=$5
   log.info "Started gitPullAll ${COL_PURPLE}${SERVER}:${PORT}:${HOST_OS}"
-  executeScriptInServer ${SERVER} ${PORT} ${IS_DOCKER_DEMO} "${HOST_OS}/git/git-pull-all.sh"
+  executeScriptInServer ${SERVER} ${PORT} ${IS_DOCKER_DEMO} "${HOST_OS}/git/git-pull-all.sh" ${IS_HTTPS}
   log.info "${COL_RED}Finished gitPullAll ${COL_CYAN}${SERVER}:${PORT}:${HOST_OS}"
 }
 
@@ -55,6 +56,12 @@ executeScriptInServer() {
   local PORT=$2
   local IS_DOCKER_DEMO=$3
   local SCRIPT=$4
+  local IS_HTTPS=$5
+  local PROTOCOL="http"
+
+  if ${IS_HTTPS}; then
+    PROTOCOL="https"
+  fi
 
   if ${IS_DOCKER_DEMO}; then
     BASIC_AUTH=${DOCKER_DEMO_GROOT_API_BASIC_AUTH}
@@ -62,7 +69,7 @@ executeScriptInServer() {
     BASIC_AUTH=${GROOT_API_BASIC_AUTH}
   fi
 
-  local URL="http://${SERVER}:${PORT}/kame-house-groot/api/v1/admin/kamehouse-shell/exec-script.php?script=${SCRIPT}"
+  local URL="${PROTOCOL}://${SERVER}:${PORT}/kame-house-groot/api/v1/admin/kamehouse-shell/exec-script.php?script=${SCRIPT}"
   log.info "Executing request: ${COL_BLUE}${URL}"
   RESPONSE=`curl --max-time 1800 -k --location --request GET "${URL}" --header "Authorization: Basic ${BASIC_AUTH}" 2>/dev/null`
   log.trace "curl response: ${RESPONSE}"
