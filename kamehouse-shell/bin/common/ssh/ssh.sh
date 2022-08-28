120e8f197e53a2772f5fb37f8add46deba390cfb
nbrest <brest.nico@gmail.com>
2022-08-28 13:28:50 +1000

    Added shortcut ssh script to ssh to my local servers

---
+++ kamehouse-shell/bin/common/ssh/ssh.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Import common functions
+source ${HOME}/programs/kamehouse-shell/bin/common/common-functions.sh
+if [ "$?" != "0" ]; then
+  echo -e "\033[1;36m$(date +%Y-%m-%d' '%H:%M:%S)\033[0;39m - [\033[1;31mERROR\033[0;39m] - \033[1;31mAn error occurred importing common-functions.sh\033[0;39m"
+  exit 1
+fi
+
+# Import kamehouse functions
+source ${HOME}/programs/kamehouse-shell/bin/common/kamehouse/kamehouse-functions.sh
+if [ "$?" != "0" ]; then
+  echo -e "\033[1;36m$(date +%Y-%m-%d' '%H:%M:%S)\033[0;39m - [\033[1;31mERROR\033[0;39m] - \033[1;31mAn error occurred importing kamehouse-functions.sh\033[0;39m"
+  exit 1
+fi
+
+mainProcess() {
+  if [ -z "${SSH_COMMAND}" ]; then
+    sshToRemoteServer
+  else
+    executeSshCommand
+  fi
+}
+
+sshToRemoteServer() {
+  log.debug "ssh -p ${SSH_PORT} -t -o ServerAliveInterval=10 ${SSH_USER}@${SSH_SERVER}"
+  ssh -p ${SSH_PORT} -t -o ServerAliveInterval=10 ${SSH_USER}@${SSH_SERVER}
+}
+
+parseArguments() {
+  parseKameHouseServer "$@"
+  
+  while getopts ":c:s:" OPT; do
+    case $OPT in
+    "c")
+      SSH_COMMAND="$OPTARG"
+      ;;
+    \?)
+      parseInvalidArgument "$OPTARG"
+      ;;
+    esac
+  done
+}
+
+setEnvFromArguments() {
+  setEnvForKameHouseServer
+  SSH_SERVER=${KAMEHOUSE_SERVER}
+}
+
+printHelpOptions() {
+  addHelpOption "-c command" "command to execute in the remote shell"
+  printKameHouseServerOption
+}
+
+main "$@"
