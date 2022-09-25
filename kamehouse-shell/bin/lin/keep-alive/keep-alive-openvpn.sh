e3bb66c0c633262e303041820d3ca309a8f994d0
nbrest <brest.nico@gmail.com>
2022-09-25 19:04:30 +1000

    Fixed keep alive openvpn

--- kamehouse-shell/bin/lin/keep-alive/keep-alive-openvpn.sh
+++ kamehouse-shell/bin/lin/keep-alive/keep-alive-openvpn.sh
@@ -15,7 +15,7 @@ SERVICE_STARTUP="sudo service openvpn start"
 
 mainProcess() {
   log.warn "User running this script needs ${COL_RED}sudo netstat${COL_DEFAULT_LOG} permissions"
-  PID=`sudo netstat -nltp | grep ":${PORT}" | awk '{print $6}' | cut -d '/' -f 1`
+  PID=`sudo netstat -nlup | grep ":${PORT}" | awk '{print $6}' | cut -d '/' -f 1`
   if [ -z ${PID} ]; then
     log.info "${SERVICE} is not running. Starting it now"
     ${SERVICE_STARTUP} &
