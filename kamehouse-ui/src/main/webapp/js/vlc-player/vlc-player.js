bfdae1425f21f1a4f91f18d5f2701e81dbe8060e
nbrest <brest.nico@gmail.com>
2023-05-14 00:03:16 +1000

    Extended vlc sync loop status

--- kamehouse-ui/src/main/webapp/js/vlc-player/vlc-player.js
+++ kamehouse-ui/src/main/webapp/js/vlc-player/vlc-player.js
@@ -860,7 +860,7 @@ function VlcPlayerSynchronizer(vlcPlayer) {
   }
 
   function syncLoopsStatus() {
-    const PERIODIC_STATUS_WAIT_MS = 28000;
+    const PERIODIC_STATUS_WAIT_MS = 30000;
     setTimeout(async () => {
       while (true) {
         const separator = "---------------------------------------------";
