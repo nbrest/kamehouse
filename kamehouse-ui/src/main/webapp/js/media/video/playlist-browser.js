9393d41ac07e42d9ea03627e3f7cc56fb5471e1a
nbrest <brest.nico@gmail.com>
2023-05-14 19:52:51 +1000

    Fixed sonar bug

--- kamehouse-ui/src/main/webapp/js/media/video/playlist-browser.js
+++ kamehouse-ui/src/main/webapp/js/media/video/playlist-browser.js
@@ -91,7 +91,7 @@ function PlaylistBrowser() {
       return string;
     }
     const arr = string.split(" ");
-    for (var i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
     }
     return arr.join(" ");
