ee311dff2c7a4f1d2c6046d27d2dee9d86087c4c
nbrest <brest.nico@gmail.com>
2023-05-30 23:14:14 +1000

    Updated sql dump file

--- docker/mysql/dump-kamehouse.sql
+++ docker/mysql/dump-kamehouse.sql
@@ -16,7 +16,9 @@ CREATE TABLE `kamehouse_user` (
   UNIQUE KEY `UK_g1flpfg6b2v863t5535afr494` (`USERNAME`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `kamehouse_user` VALUES (1,_binary '',_binary '',_binary '','seiya@saintseiya.com',_binary '','Seiya',NULL,'Saint','$2a$12$FEV4fpmaooeZOoth0v9B9OVLmMbsy0jJONtKE4Y03yOpxQ0zGAtw.','seiya'),(2,_binary '',_binary '',_binary '','ryoma@pot.com',_binary '','Ryoma',NULL,'Echizen','$2a$12$gZ3HukIieOPe6/zK.fEsAe8k4Y2kdeEwsiskJ21p4Ev67IxuugENe','ryoma'),(3,_binary '',_binary '',_binary '','vegeta@dbz.com',_binary '','Vegeta',NULL,'Prince','$2a$12$cvGXhFqnXkeE0p3KUThwmO4BIxtKXHBcmCaq/gJQO.n9pc.O7pBPi','vegeta');
+INSERT INTO `kamehouse_user` VALUES (1,_binary '',_binary '',_binary '','seiya@saintseiya.com',_binary '','Seiya',NULL,'Saint','$2a$12$FEV4fpmaooeZOoth0v9B9OVLmMbsy0jJONtKE4Y03yOpxQ0zGAtw.','seiya'),
+  (2,_binary '',_binary '',_binary '','ryoma@pot.com',_binary '','Ryoma',NULL,'Echizen','$2a$12$gZ3HukIieOPe6/zK.fEsAe8k4Y2kdeEwsiskJ21p4Ev67IxuugENe','ryoma'),
+  (3,_binary '',_binary '',_binary '','vegeta@dbz.com',_binary '','Vegeta',NULL,'Prince','$2a$12$cvGXhFqnXkeE0p3KUThwmO4BIxtKXHBcmCaq/gJQO.n9pc.O7pBPi','vegeta');
 
 DROP TABLE IF EXISTS `kamehouse_role`;
 CREATE TABLE `kamehouse_role` (
@@ -28,7 +30,10 @@ CREATE TABLE `kamehouse_role` (
   CONSTRAINT `FK1aug04ikis5or4g40omwb5586` FOREIGN KEY (`kamehouse_user_id`) REFERENCES `kamehouse_user` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `kamehouse_role` VALUES (1,'ROLE_SAIYAJIN',1),(2,'ROLE_KAMISAMA',1),(3,'ROLE_SAIYAJIN',2),(4,'ROLE_NAMEKIAN',3);
+INSERT INTO `kamehouse_role` VALUES (1,'ROLE_SAIYAJIN',1),
+  (2,'ROLE_KAMISAMA',1),
+  (3,'ROLE_SAIYAJIN',2),
+  (4,'ROLE_NAMEKIAN',3);
 
 DROP TABLE IF EXISTS `vlc_player`;
 CREATE TABLE `vlc_player` (
