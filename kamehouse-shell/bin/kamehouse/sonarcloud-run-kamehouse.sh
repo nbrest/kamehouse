1552eaeed9ee1658edb0395b9d0c082740211bb3
nbrest <brest.nico@gmail.com>
2023-11-09 19:30:30 +1100

    Added sonarcloud scan run script

---
+++ kamehouse-shell/bin/kamehouse/sonarcloud-run-kamehouse.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Import common functions
+source ${HOME}/programs/kamehouse-shell/bin/common/common-functions.sh
+if [ "$?" != "0" ]; then
+  echo -e "\033[1;36m$(date +%Y-%m-%d' '%H:%M:%S)\033[0;39m - [\033[1;31mERROR\033[0;39m] - \033[1;31mAn error occurred importing common-functions.sh\033[0;39m"
+  exit 1
+fi
+
+# Import kamehouse functions
+source ${HOME}/programs/kamehouse-shell/bin/common/kamehouse/kamehouse-functions.sh
+if [ "$?" != "0" ]; then
+  echo -e "\033[1;36m$(date +%Y-%m-%d' '%H:%M:%S)\033[0;39m - [\033[1;31mERROR\033[0;39m] - \033[1;31mAn error occurred importing kamehouse-functions.sh\033[0;39m"
+  exit 1
+fi
+source ${HOME}/.kamehouse/.shell/.cred
+
+LOG_PROCESS_TO_FILE=true
+
+mainProcess() {
+  log.info "Running sonarcloud scan"
+  log.trace "SONAR_TOKEN=${SONAR_TOKEN}"
+  mvn -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=nbrest_kamehouse
+}
+
+main "$@"
