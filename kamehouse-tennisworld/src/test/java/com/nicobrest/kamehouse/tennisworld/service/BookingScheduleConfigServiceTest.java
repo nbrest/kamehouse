ab480576a5e5ee40aa0301e07d82565d313ca9f8
nbrest <brest.nico@gmail.com>
2023-11-28 19:41:07 +1100

    Added unit tests

--- kamehouse-tennisworld/src/test/java/com/nicobrest/kamehouse/tennisworld/service/BookingScheduleConfigServiceTest.java
+++ kamehouse-tennisworld/src/test/java/com/nicobrest/kamehouse/tennisworld/service/BookingScheduleConfigServiceTest.java
@@ -1,15 +1,18 @@
 package com.nicobrest.kamehouse.tennisworld.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import com.nicobrest.kamehouse.commons.dao.CrudDao;
+import com.nicobrest.kamehouse.commons.exception.KameHouseInvalidDataException;
 import com.nicobrest.kamehouse.commons.service.AbstractCrudServiceTest;
 import com.nicobrest.kamehouse.commons.service.CrudService;
 import com.nicobrest.kamehouse.commons.testutils.TestUtils;
 import com.nicobrest.kamehouse.tennisworld.model.BookingScheduleConfig;
 import com.nicobrest.kamehouse.tennisworld.model.dto.BookingScheduleConfigDto;
 import com.nicobrest.kamehouse.tennisworld.testutils.BookingScheduleConfigTestUtils;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -49,5 +52,56 @@ class BookingScheduleConfigServiceTest
   @Override
   public TestUtils<BookingScheduleConfig, BookingScheduleConfigDto> getTestUtils() {
     return new BookingScheduleConfigTestUtils();
+  }
+
+  /**
+   * Tests invalid time in the configuration.
+   */
+  @Test
+  void validateInvalidTimeTest() {
+    BookingScheduleConfig bookingScheduleConfig = testUtils.getSingleTestData();
+    bookingScheduleConfig.setTime(null);
+    assertThrows(
+        KameHouseInvalidDataException.class,
+        () -> bookingScheduleConfigService.validate(bookingScheduleConfig)
+    );
+
+    bookingScheduleConfig.setTime("invalid-time");
+    assertThrows(
+        KameHouseInvalidDataException.class,
+        () -> bookingScheduleConfigService.validate(bookingScheduleConfig)
+    );
+  }
+
+  /**
+   * Tests invalid bookAheadDays in the configuration.
+   */
+  @Test
+  void validateInvalidBookAheadDaysTest() {
+    BookingScheduleConfig bookingScheduleConfig = testUtils.getSingleTestData();
+    bookingScheduleConfig.setBookAheadDays(null);
+    assertThrows(
+        KameHouseInvalidDataException.class,
+        () -> bookingScheduleConfigService.validate(bookingScheduleConfig)
+    );
+
+    bookingScheduleConfig.setBookAheadDays(-10);
+    assertThrows(
+        KameHouseInvalidDataException.class,
+        () -> bookingScheduleConfigService.validate(bookingScheduleConfig)
+    );
+  }
+
+  /**
+   * Tests invalid duration in the configuration.
+   */
+  @Test
+  void validateInvalidDurationTest() {
+    BookingScheduleConfig bookingScheduleConfig = testUtils.getSingleTestData();
+    bookingScheduleConfig.setDuration("invalid-duration");
+    assertThrows(
+        KameHouseInvalidDataException.class,
+        () -> bookingScheduleConfigService.validate(bookingScheduleConfig)
+    );
   }
 }
