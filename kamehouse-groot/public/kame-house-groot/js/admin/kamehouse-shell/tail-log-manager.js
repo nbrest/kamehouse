57a8b76f34e504d37e86622e31c5ded29e5da6c7
nbrest <brest.nico@gmail.com>
2023-12-06 07:59:51 +1100

    Moved TailLogManager to a class

--- kamehouse-groot/public/kame-house-groot/js/admin/kamehouse-shell/tail-log-manager.js
+++ kamehouse-groot/public/kame-house-groot/js/admin/kamehouse-shell/tail-log-manager.js
@@ -1,23 +1,22 @@
 /**
  * Manager to tail logs in the current server.
+ * 
+ * @author nbrest
  */
-function TailLogManager() {
+class TailLogManager {
 
-  this.load = load;
-  this.tailLog = tailLog;
-
-  const KAMEHOUSE_SHELL_EXECUTE_API = '/kame-house-groot/api/v1/admin/kamehouse-shell/execute.php';
+  static #KAMEHOUSE_SHELL_EXECUTE_API = '/kame-house-groot/api/v1/admin/kamehouse-shell/execute.php';
 
   /**
    * Load the extension.
    */
-  function load() {
+  load() {
     kameHouse.logger.info("Initialized tailLogManager");
   }
 
   /** Tails the log based on the script parameter and the number of lines to display */
-  function tailLog(scriptName, numberOfLines, logLevel, executeOnDockerHost, callback) {
-    if (isValidScript(scriptName)) {
+  tailLog(scriptName, numberOfLines, logLevel, executeOnDockerHost, callback) {
+    if (this.#isValidScript(scriptName)) {
       kameHouse.logger.trace("Executing script : " + scriptName);
       if (kameHouse.core.isEmpty(logLevel)) {
         logLevel = "";
@@ -29,20 +28,20 @@ function TailLogManager() {
       };
       const config = kameHouse.http.getConfig();
       config.timeout = 20;
-      kameHouse.http.get(config, KAMEHOUSE_SHELL_EXECUTE_API, kameHouse.http.getUrlEncodedHeaders(), params,
-        (responseBody, responseCode, responseDescription, responseHeaders) => updateTailLogOutput(responseBody, responseCode, responseDescription, responseHeaders, numberOfLines, callback),
-        (responseBody, responseCode, responseDescription, responseHeaders) => updateTailLogOutputError(responseBody, responseCode, responseDescription, responseHeaders, callback));
+      kameHouse.http.get(config, TailLogManager.#KAMEHOUSE_SHELL_EXECUTE_API, kameHouse.http.getUrlEncodedHeaders(), params,
+        (responseBody, responseCode, responseDescription, responseHeaders) => this.#updateTailLogOutput(responseBody, responseCode, responseDescription, responseHeaders, numberOfLines, callback),
+        (responseBody, responseCode, responseDescription, responseHeaders) => this.#updateTailLogOutputError(responseBody, responseCode, responseDescription, responseHeaders, callback));
     } else {
       const message = "Invalid or no script received as url parameter";
       kameHouse.logger.error(message, kameHouse.logger.getRedText(message));
-      displayInvalidScript();
+      this.#displayInvalidScript();
     }
   }
 
   /**
    * Check if it's a valid script.
    */
-  function isValidScript(scriptName) {
+  #isValidScript(scriptName) {
     if (!kameHouse.core.isEmpty(scriptName)) { 
       if (scriptName.startsWith("common/logs/cat-") && scriptName.endsWith("-log.sh")) {
         return true;
@@ -55,22 +54,22 @@ function TailLogManager() {
   }
 
   /** Update the script tail log output with the result of the script */
-  function updateTailLogOutput(responseBody, responseCode, responseDescription, responseHeaders, numberOfLines, callback) {
+  #updateTailLogOutput(responseBody, responseCode, responseDescription, responseHeaders, numberOfLines, callback) {
     const tailLogOutputArray = responseBody.htmlConsoleOutput;
     const $tailLogOutputTableBody = $('#tail-log-output-table-body');  
-    const tbody = getTailLogOutputTbody();
+    const tbody = this.#getTailLogOutputTbody();
     const tailLogOutputLength = tailLogOutputArray.length;
     if (tailLogOutputLength < numberOfLines) {
       // Show full output
       for (let i = 0; i < tailLogOutputLength; i++) {
         if (tailLogOutputArray[i].trim().length > 0) {
-          kameHouse.util.dom.append(tbody, getTailLogOutputTr(tailLogOutputArray[i]));
+          kameHouse.util.dom.append(tbody, this.#getTailLogOutputTr(tailLogOutputArray[i]));
         }
       }
     } else {
       for (let i = tailLogOutputLength - numberOfLines; i < tailLogOutputLength; i++) {
         if (tailLogOutputArray[i].trim().length > 0) {
-          kameHouse.util.dom.append(tbody, getTailLogOutputTr(tailLogOutputArray[i]));
+          kameHouse.util.dom.append(tbody, this.#getTailLogOutputTr(tailLogOutputArray[i]));
         }
       }
     }
@@ -83,13 +82,13 @@ function TailLogManager() {
   }
 
   /** Displays the error message in the tail log output */
-  function updateTailLogOutputError(responseBody, responseCode, responseDescription, responseHeaders, callback) {
+  #updateTailLogOutputError(responseBody, responseCode, responseDescription, responseHeaders, callback) {
     if ((responseCode == "0" && responseDescription == "timeout") || responseCode == "-4") {
       kameHouse.logger.warn("Tail log request timed out");
     } else {
       const tbody = $("#tail-log-output-table-body");
       const errorMessage = kameHouse.logger.getCyanText(kameHouse.util.time.getTimestamp()) + " - [" + kameHouse.logger.getRedText("ERROR") + "] - " + kameHouse.logger.getRedText("Error response from the backend. responseCode : '" + responseCode + "'. responseBody : '" + responseBody + "'. responseDescription : '" + responseDescription + "'");
-      kameHouse.util.dom.append(tbody, getTailLogOutputErrorTr(errorMessage));
+      kameHouse.util.dom.append(tbody, this.#getTailLogOutputErrorTr(errorMessage));
     }
 
     if (kameHouse.core.isFunction(callback)) {
@@ -98,10 +97,10 @@ function TailLogManager() {
   }
 
   /** Displays the error message in the tail log output from an invalid script */
-  function displayInvalidScript() {
+  #displayInvalidScript() {
     const $tailLogOutputTableBody = $('#tail-log-output-table-body');
-    const tbody = getTailLogOutputTbody();
-    kameHouse.util.dom.append(tbody, getTailLogOutputErrorTr("Invalid script sent as parameter"));
+    const tbody = this.#getTailLogOutputTbody();
+    kameHouse.util.dom.append(tbody, this.#getTailLogOutputErrorTr("Invalid script sent as parameter"));
     kameHouse.util.dom.empty($tailLogOutputTableBody);
     kameHouse.util.dom.replaceWith($tailLogOutputTableBody, tbody);
   }
@@ -109,7 +108,7 @@ function TailLogManager() {
   /**
    * Get tail log output table body.
    */
-  function getTailLogOutputTbody() {
+  #getTailLogOutputTbody() {
     return kameHouse.util.dom.getTbody({
       id: "tail-log-output-table-body"
     }, null);
@@ -118,14 +117,14 @@ function TailLogManager() {
   /**
    * Get tail log output error table row.
    */
-  function getTailLogOutputErrorTr(message) {
+  #getTailLogOutputErrorTr(message) {
     return kameHouse.util.dom.getTrTd(message);
   }
 
   /**
    * Get tail log output table row.
    */
-  function getTailLogOutputTr(htmlContent) {
+  #getTailLogOutputTr(htmlContent) {
     return kameHouse.util.dom.getTrTd(htmlContent);
   }
 }
