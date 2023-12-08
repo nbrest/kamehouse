c681fcf5b0ba13a47215aeb54ead28f0a7bba1a4
nbrest <brest.nico@gmail.com>
2023-12-08 21:48:52 +1100

    Updated kamehouse chrome extension to use classes

--- chrome-extensions/kamehouse/kamehouse-chrome-extension.js
+++ chrome-extensions/kamehouse/kamehouse-chrome-extension.js
@@ -1,43 +1,76 @@
-console.log("KameHouse Chrome Extension background process");
+/**
+ * KameHouse Chrome Extension loader.
+ * 
+ * @author nbrest
+ */
+try {
+  class KameHouseChromeExtensionLoader {
 
-// Execute KameHouse Chrome Extension when I click the extension button
-chrome.action.onClicked.addListener(function (tab) {
-  console.log("Executing kamehouse on action.onClicked");
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['kamehouse-inject.js']	
-  });
-  chrome.scripting.insertCSS({
-    target: { tabId: tab.id },
-    files: ["kamehouse-inject.css"]
-  });
-});
-
-// Execute KameHouse Chrome Extension when I open a tab/page
-chrome.tabs.onCreated.addListener((tab) => {
-  console.log("Executing kamehouse on tabs.onCreated");
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['kamehouse-inject.js']	
-  });
-  chrome.scripting.insertCSS({
-    target: { tabId: tab.id },
-    files: ["kamehouse-inject.css"]
-  });
-});
-
-// Execute KameHouse Chrome Extension when I refresh a tab/page
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete") {
-    console.log("Executing kamehouse on tab.onUpdated");
-    chrome.scripting.executeScript({
-      target: {tabId: tabId},
-      files: ['kamehouse-inject.js']	
-    });
-    chrome.scripting.insertCSS({
-      target: { tabId: tabId },
-      files: ["kamehouse-inject.css"]
-    });
-  }
-});
-
+    load() {
+      console.log(this.#getTimestamp() + " KameHouse Chrome Extension background process");
+      this.#addListeners();
+    }
+  
+    /**
+     * Add listeners that trigger the code injection.
+     */
+    #addListeners() {
+      console.log(this.#getTimestamp() + " Setting KameHouse Chrome Extension listeners");
+      // Execute KameHouse Chrome Extension when I click the extension button
+      chrome.action.onClicked.addListener((tab) => {
+        console.log(this.#getTimestamp() + " Executing kamehouse on action.onClicked");
+        chrome.scripting.executeScript({
+          target: {tabId: tab.id},
+          files: ['kamehouse-inject.js']	
+        });
+        chrome.scripting.insertCSS({
+          target: { tabId: tab.id },
+          files: ["kamehouse-inject.css"]
+        });
+      });
+      
+      // Execute KameHouse Chrome Extension when I open a tab/page
+      chrome.tabs.onCreated.addListener((tab) => {
+        console.log(this.#getTimestamp() + " Executing kamehouse on tabs.onCreated");
+        chrome.scripting.executeScript({
+          target: {tabId: tab.id},
+          files: ['kamehouse-inject.js']	
+        });
+        chrome.scripting.insertCSS({
+          target: { tabId: tab.id },
+          files: ["kamehouse-inject.css"]
+        });
+      });
+      
+      // Execute KameHouse Chrome Extension when I refresh a tab/page
+      chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+        if (changeInfo.status === "complete") {
+          console.log(this.#getTimestamp() + " Executing kamehouse on tab.onUpdated");
+          chrome.scripting.executeScript({
+            target: {tabId: tabId},
+            files: ['kamehouse-inject.js']	
+          });
+          chrome.scripting.insertCSS({
+            target: { tabId: tabId },
+            files: ["kamehouse-inject.css"]
+          });
+        }
+      });
+    }
+  
+    /**
+     * Get current timestamp.
+     */
+    #getTimestamp() {
+      const date = new Date();
+      const offsetTime = date.getTimezoneOffset() * -1 * 60 * 1000;
+      const currentDateTime = date.getTime();
+      return new Date(currentDateTime + offsetTime).toISOString().replace("T"," ").slice(0,19);
+    }  
+  
+  } // KameHouseChromeExtensionLoader
+  
+  new KameHouseChromeExtensionLoader().load();  
+} catch(error) {
+  console.log(error);
+}
--- chrome-extensions/kamehouse/kamehouse-inject.js
+++ chrome-extensions/kamehouse/kamehouse-inject.js
@@ -1,61 +1,89 @@
-console.log("KameHouse Chrome Extension Script");
-
-function main() {
-  setGlobalVariables();
-  //createAutoCloseableModal();
-  //testFunctionLoop();
-}
-
-function setGlobalVariables() {
-  if (!window.kameHouseGlobal) {
-    console.log("Setting KameHouse global variables");
-    window.kameHouseGlobal = 1;
-  } else {
-    console.log("KameHouse global variables already set");
-  }  
-}
-
-function testFunctionLoop() {
-  let count = 1;
-  while (count <= 5) {
-    setTimeout(testFunction, 5000 + count * 5000);
-    count = count + 1;
-  }
-}
-
-function testFunction() {
-  console.log(getTimestamp() + " : window.kameHouseGlobal " + window.kameHouseGlobal);
-}
-
-function createAutoCloseableModal() {
-  //scrollToTop();
-  console.log("Creating KameHouse Chrome extension modal");
-  const modal = document.createElement("div");
-  modal.setAttribute("id", "kamehouse-chrome-extension-div");
-  document.body.appendChild(modal);
-  modal.innerHTML = getTimestamp() + " - KameHouse Chrome Extension is running<br><br>Auto closing in 5 seconds";
-  setTimeout(()=> {
-    console.log("Hiding KameHouse Chrome extension modal")
-    modal.style.display = "none";
-  }, 5000);
-}
-
-function getTimestamp() {
-  const date = new Date();
-  const offsetTime = date.getTimezoneOffset() * -1 * 60 * 1000;
-  const currentDateTime = date.getTime();
-  return new Date(currentDateTime + offsetTime).toISOString().replace("T"," ").slice(0,19);
-}
-
-function scrollToTop() {
-  document.body.scrollTop = document.documentElement.scrollTop = 0;
-}
-
 /**
- * Execute existing function in page:
-(function() {
-  location.href="javascript:someExistingFunctionInPage(); void 0";
-})();
+ * KameHouse Chrome Extension.
+ * 
+ * @author nbrest
  */
+try {
+  class KameHouseChromeExtension {
 
-main();
+    /**
+     * Load the chrome extension.
+     */
+    load() {
+      console.log(this.getTimestamp() + " Loading KameHouse Chrome Extension");
+      this.setGlobalVariables();
+      //this.createAutoCloseableModal();
+      this.testFunctionLoop();
+    }
+    
+    /**
+     * Set global variables.
+     */
+    setGlobalVariables() {
+      if (!window.kameHouseGlobal) {
+        console.log(this.getTimestamp() + " Setting KameHouse global variables");
+        window.kameHouseGlobal = 1;
+      } else {
+        console.log(this.getTimestamp() + " KameHouse global variables already set");
+      }  
+    }
+    
+    /**
+     * Run test function.
+     */
+    testFunctionLoop() {
+      let count = 1;
+      while (count <= 3) {
+        setTimeout(() => this.testFunction(), 5000 + count * 5000);
+        count = count + 1;
+      }
+    }
+    
+    /**
+     * Test function.
+     */
+    testFunction() {
+      console.log(this.getTimestamp() + " : window.kameHouseGlobal " + window.kameHouseGlobal);
+    }
+    
+    /**
+     * Create an auto closeable modal.
+     */
+    createAutoCloseableModal() {
+      console.log(this.getTimestamp() + " Creating KameHouse Chrome extension modal");
+      const modal = document.createElement("div");
+      modal.setAttribute("id", "kamehouse-chrome-extension-div");
+      document.body.appendChild(modal);
+      modal.innerHTML = this.getTimestamp() + " - KameHouse Chrome Extension is running<br><br>Auto closing in 5 seconds";
+      setTimeout(()=> {
+        console.log(this.getTimestamp() + " Hiding KameHouse Chrome extension modal")
+        modal.style.display = "none";
+      }, 5000);
+    }
+    
+    /**
+     * Get current timestamp.
+     */
+    getTimestamp() {
+      const date = new Date();
+      const offsetTime = date.getTimezoneOffset() * -1 * 60 * 1000;
+      const currentDateTime = date.getTime();
+      return new Date(currentDateTime + offsetTime).toISOString().replace("T"," ").slice(0,19);
+    }
+    
+    /**
+     * Scroll to the top.
+     */
+    scrollToTop() {
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+    }
+    
+  } // KameHouseChromeExtension
+
+  new KameHouseChromeExtension().load();
+} catch(error) {
+  console.log(error);
+}
+
+
+
