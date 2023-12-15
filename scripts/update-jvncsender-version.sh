8e504d0d62ea5c91a71ea03eaecc77c1c818a2a5
nbrest <brest.nico@gmail.com>
2023-12-15 18:27:57 +1100

    Added script to update jvncsender version

---
+++ scripts/update-jvncsender-version.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Import common functions
+source ${HOME}/programs/kamehouse-shell/bin/common/common-functions.sh
+if [ "$?" != "0" ]; then
+  echo -e "\033[1;36m$(date +%Y-%m-%d' '%H:%M:%S)\033[0;39m - [\033[1;31mERROR\033[0;39m] - \033[1;31mAn error occurred importing common-functions.sh\033[0;39m"
+  exit 1
+fi
+
+# Import kamehouse functions
+source ${HOME}/programs/kamehouse-shell/bin/common/kamehouse/kamehouse-functions.sh
+if [ "$?" != "0" ]; then
+  echo -e "\033[1;36m$(date +%Y-%m-%d' '%H:%M:%S)\033[0;39m - [\033[1;31mERROR\033[0;39m] - \033[1;31mAn error occurred importing kamehouse-functions.sh\033[0;39m"
+  exit 1
+fi
+
+LOG_PROCESS_TO_FILE=true
+RELEASE_VERSION=""
+
+mainProcess() {
+  checkCurrentDir
+  updateJvncSenderVersion
+}
+
+updateJvncSenderVersion() {
+  rm -rfv ./local-maven-repo/be/jedi/jvncsender
+  rm -rfv ${HOME}/temp/jvncsender
+  mkdir -pv ${HOME}/temp/jvncsender
+  cp -v ${HOME}/.m2/repository/be/jedi/jvncsender/${RELEASE_VERSION}-SNAPSHOT/jvncsender-${RELEASE_VERSION}-SNAPSHOT-jar-with-dependencies.jar ${HOME}/temp/jvncsender/jvncsender-${RELEASE_VERSION}.jar
+  cp ./scripts/jvncsender.pom.xml ${HOME}/temp/jvncsender/jvncsender-${RELEASE_VERSION}.pom.xml
+  sed -i "s+RELEASE_VERSION+${RELEASE_VERSION}+g" ${HOME}/temp/jvncsender/jvncsender-${RELEASE_VERSION}.pom.xml
+
+  mvn deploy:deploy-file -Dstyle.color=always -DgroupId=be.jedi -DartifactId=jvncsender -Dversion=${RELEASE_VERSION} -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=${HOME}/temp/jvncsender/jvncsender-${RELEASE_VERSION}.jar -DpomFile=${HOME}/temp/jvncsender/jvncsender-${RELEASE_VERSION}.pom.xml
+
+  rm -rfv ${HOME}/temp/jvncsender
+}
+
+checkCurrentDir() {
+  if [ ! -d "./kamehouse-shell/bin" ] || [ ! -d "./.git" ]; then
+    log.error "This script needs to run from the root directory of a kamehouse git repository. Can't continue"
+    exitProcess 1
+  fi
+}
+
+parseArguments() {
+  while getopts ":v:" OPT; do
+    case $OPT in 
+    ("v")
+      RELEASE_VERSION="$OPTARG"
+      ;;        
+    (\?)
+      parseInvalidArgument "$OPTARG"
+      ;;
+    esac
+  done
+}
+
+setEnvFromArguments() {
+  if [ -z "${RELEASE_VERSION}" ]; then
+    log.error "Option -v is required"
+    printHelp
+    exitProcess 1
+  fi
+}
+
+printHelpOptions() {
+  addHelpOption "-v [x.xx]" "jvncsender release version"
+}
+
+main "$@"
