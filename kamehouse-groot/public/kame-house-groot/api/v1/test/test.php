7c46cd0e12c778bbff787f52e2d77baed4d8491b
nbrest <brest.nico@gmail.com>
2024-02-16 21:19:59 +1100

    Fixed broken test

--- kamehouse-groot/public/kame-house-groot/api/v1/test/test.php
+++ kamehouse-groot/public/kame-house-groot/api/v1/test/test.php
@@ -16,6 +16,8 @@ class TestApi {
    */
   public function main() {
     require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . "/kame-house-groot/api/v1/kamehouse/commons/kamehouse.php");
+    require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . "/kame-house-groot/api/v1/kamehouse/test/test.php");
+    require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . "/kame-house-groot/api/v1/kamehouse/commons/examples/examples.php");
     $kameHouseTest = new KameHouseTest();
     $kameHouseTest->runAll();
     $kameHouseExamples = new KameHouseExamples();
