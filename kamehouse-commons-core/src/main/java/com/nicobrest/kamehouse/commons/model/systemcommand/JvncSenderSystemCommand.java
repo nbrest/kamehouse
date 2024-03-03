b6be09a2127f7877bc284e5a9167dba88de4aa6f
nbrest <brest.nico@gmail.com>
2024-03-03 13:06:02 +1100

    Updated constructor visibility

--- kamehouse-commons-core/src/main/java/com/nicobrest/kamehouse/commons/model/systemcommand/JvncSenderSystemCommand.java
+++ kamehouse-commons-core/src/main/java/com/nicobrest/kamehouse/commons/model/systemcommand/JvncSenderSystemCommand.java
@@ -26,7 +26,7 @@ public abstract class JvncSenderSystemCommand extends SystemCommand {
   /**
    * Set the output command.
    */
-  public JvncSenderSystemCommand() {
+  protected JvncSenderSystemCommand() {
     setOutputCommand();
   }
 
