bfec29541cb7b85405d9527dae50b089ed6a79d5
nbrest <brest.nico@gmail.com>
2024-03-09 14:07:15 +1100

    Moved groot to src/main/webapp

--- scripts/setup-apache-httpd-dirs-eclipse.bat
+++ scripts/setup-apache-httpd-dirs-eclipse.bat
@@ -6,7 +6,7 @@ rmdir "%USERPROFILE%\programs\apache-httpd\www\www-eclipse\kame-house"
 mklink /D "%USERPROFILE%\programs\apache-httpd\www\www-eclipse\kame-house" "%USERPROFILE%\workspace-eclipse\kamehouse\kamehouse-ui\src\main\webapp"
 
 rmdir "%USERPROFILE%\programs\apache-httpd\www\www-eclipse\kame-house-groot"
-mklink /D "%USERPROFILE%\programs\apache-httpd\www\www-eclipse\kame-house-groot" "%USERPROFILE%\workspace-eclipse\kamehouse\kamehouse-groot\public\kame-house-groot"
+mklink /D "%USERPROFILE%\programs\apache-httpd\www\www-eclipse\kame-house-groot" "%USERPROFILE%\workspace-eclipse\kamehouse\kamehouse-groot\src\main\webapp\kame-house-groot"
 
 rmdir "%USERPROFILE%\programs\apache-httpd\www\www-eclipse\kame-house-mobile"
 mklink /D "%USERPROFILE%\programs\apache-httpd\www\www-eclipse\kame-house-mobile" "%USERPROFILE%\workspace-eclipse\kamehouse\kamehouse-eclipse\www\kame-house-mobile"
