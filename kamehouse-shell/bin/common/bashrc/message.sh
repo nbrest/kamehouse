4c4dd14103afe7cbcc6436cf457d27a464c5cb50
nbrest <brest.nico@gmail.com>
2024-04-08 20:27:01 +1000

    Fixed shell for scp

--- kamehouse-shell/bin/common/bashrc/message.sh
+++ kamehouse-shell/bin/common/bashrc/message.sh
@@ -1,12 +1,14 @@
 ### Message every time I open a shell
-if (( $EUID != 0 )); then
-  # Normal user
-  echo ""
-  echo -e "                          ${COL_RED}まだまだだね${COL_NORMAL}"
-  echo ""
-else
-  # root
-  echo ""
-  echo -e "                          ${COL_BLUE}pegasus ryu sei ken${COL_NORMAL}"
-  echo ""
+if shopt -q login_shell; then
+  if (( $EUID != 0 )); then
+    # Normal user
+    echo ""
+    echo -e "                          ${COL_RED}まだまだだね${COL_NORMAL}"
+    echo ""
+  else
+    # root
+    echo ""
+    echo -e "                          ${COL_BLUE}pegasus ryu sei ken${COL_NORMAL}"
+    echo ""
+  fi
 fi
