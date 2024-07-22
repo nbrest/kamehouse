9f0c3b2406b2d49b80bfee0c14a2e140d599289f
nbrest <brest.nico@gmail.com>
2024-07-23 07:31:31 +1000

    Refactored apk status iframe message listener

--- kamehouse-ui/src/main/typescript/js/downloads.ts
+++ kamehouse-ui/src/main/typescript/js/downloads.ts
@@ -24,19 +24,20 @@ class DownloadsManager {
     window.addEventListener(
       "message",
       (event) => {
-        if (event.origin == "https://kame.nicobrest.com") {
-          const data = event.data;
-          kameHouse.logger.trace("message event data: " + kameHouse.json.stringify(data, null, 2), null);
-          if (data == "apk-status-loaded") {
-            kameHouse.logger.info("APK Status iframe loaded", null);
-            const apkStatusIframe = document.getElementById("apk-status-iframe") as HTMLIFrameElement;
-            kameHouse.util.dom.classListRemove(apkStatusIframe, "hidden-kh");
-            const apkStatusNotAvailableDiv = document.getElementById("apk-status-not-available");
-            kameHouse.util.dom.classListAdd(apkStatusNotAvailableDiv, "hidden-kh");
-          }
-        } else {
+        if (event.origin != "https://kame.nicobrest.com") {
           kameHouse.logger.trace("Ignoring message from unexpected origin", null);
+          return;
         }
+        const data = event.data;
+        kameHouse.logger.trace("message event data: " + kameHouse.json.stringify(data, null, 2), null);
+        if (data != "apk-status-loaded") {
+          return;
+        }
+        kameHouse.logger.info("APK status iframe loaded", null);
+        const apkStatusIframe = document.getElementById("apk-status-iframe") as HTMLIFrameElement;
+        kameHouse.util.dom.classListRemove(apkStatusIframe, "hidden-kh");
+        const apkStatusNotAvailableDiv = document.getElementById("apk-status-not-available");
+        kameHouse.util.dom.classListAdd(apkStatusNotAvailableDiv, "hidden-kh");
       },
       false,
     );
