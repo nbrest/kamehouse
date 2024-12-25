9f6fd433a537a23d76d266e76e8912ed9c51bf00
nbrest <brest.nico@gmail.com>
2024-12-26 08:49:10 +1100

    Updated websocket configuration for vlc player

--- kamehouse-vlcrc/src/main/java/com/nicobrest/kamehouse/vlcrc/config/VlcRcWebSocketConfig.java
+++ kamehouse-vlcrc/src/main/java/com/nicobrest/kamehouse/vlcrc/config/VlcRcWebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 /**
  * Configuration class for all web sockets in the application.
@@ -29,5 +30,13 @@ public class VlcRcWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     registry.addEndpoint("/api/ws/vlc-player/playlist")
         .setAllowedOriginPatterns("*")
         .withSockJS();
+  }
+
+  @Override
+  public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+    registry.setMessageSizeLimit(10000 * 1024);
+    registry.setSendBufferSizeLimit(10000 * 1024);
+    registry.setSendTimeLimit(20000);
+    registry.setTimeToFirstMessage(20000);
   }
 }
