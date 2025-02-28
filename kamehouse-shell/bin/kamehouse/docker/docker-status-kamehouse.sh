ac415cd1bc1cac1656fe2183e236b06b4fffc618
nbrest <brest.nico@gmail.com>
2025-03-01 10:32:20 +1100

    Removed docker server status check

--- kamehouse-shell/bin/kamehouse/docker/docker-status-kamehouse.sh
+++ kamehouse-shell/bin/kamehouse/docker/docker-status-kamehouse.sh
@@ -20,9 +20,6 @@ if [ "$?" != "0" ]; then
   exit 99
 fi
 
-KAMEHOUSE_SERVER="${DOCKER_SERVER}"
-SKIP_KAMEHOUSE_SERVER_CHECK=false
-
 mainProcess() {
   log.info "Checking docker status on current server ${COL_PURPLE}${HOSTNAME}"
   echo ""
@@ -39,40 +36,6 @@ mainProcess() {
   log.info "Docker volumes"
   echo ""
   docker volume ls
-
-  if ! ${SKIP_KAMEHOUSE_SERVER_CHECK}; then
-    kameHouseDockerContainersServerStatus
-  fi
-}
-
-kameHouseDockerContainersServerStatus() {
-  if [ "${KAMEHOUSE_SERVER}" != "${HOSTNAME}" ]; then
-    log.info "Checking docker status on kamehouse docker containers server ${COL_PURPLE}${KAMEHOUSE_SERVER}"
-    setSshParameters
-    executeSshCommand
-  fi
-}
-
-setSshParameters() {
-  setEnvForKameHouseServer
-  SSH_COMMAND="~/programs/kamehouse-shell/bin/kamehouse/docker/docker-status-kamehouse.sh"
-}
-
-parseArguments() {
-  while getopts ":s" OPT; do
-    case $OPT in
-    ("s")
-      SKIP_KAMEHOUSE_SERVER_CHECK=true
-      ;;
-    (\?)
-      parseInvalidArgument "$OPTARG"
-      ;;
-    esac
-  done 
-}
-
-printHelpOptions() {
-  addHelpOption "-s" "skip docker server status check"
 }
 
 main "$@"
