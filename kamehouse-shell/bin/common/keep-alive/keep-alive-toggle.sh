33bc45479bd7929ffbde92431ea5f5af45d5e0fc
nbrest <brest.nico@gmail.com>
2025-07-12 08:39:09 +1000

    Refactored keep alive toggle script

--- kamehouse-shell/bin/common/keep-alive/keep-alive-toggle.sh
+++ kamehouse-shell/bin/common/keep-alive/keep-alive-toggle.sh
@@ -4,22 +4,22 @@ source ${HOME}/programs/kamehouse-shell/bin/common/functions/kamehouse/kamehouse
 if [ "$?" != "0" ]; then echo "Error importing kamehouse-functions.sh" ; exit 99 ; fi
 
 initScriptEnv() {
-  SERVICE="kamehouse-desktop"
-  SERVICE_STARTUP="${HOME}/programs/kamehouse-shell/bin/kamehouse/desktop/kamehouse-desktop-startup.sh"
+  KAMEHOUSE_CFG="${HOME}/.kamehouse/config/kamehouse.cfg"
 }
 
 mainProcess() {
   if ${KEEP_ALIVE_SCRIPTS_DISABLED}; then
     log.info "keep alive scripts are ${COL_RED}disabled${COL_DEFAULT_LOG} in kamehouse.cfg. Switching to ${COL_YELLOW}enabled"
-    sed -i -E "s/^#KEEP_ALIVE_SCRIPTS_DISABLED=.*/KEEP_ALIVE_SCRIPTS_DISABLED=/I" ${HOME}/.kamehouse/config/kamehouse.cfg
-    sed -i -E "s/^KEEP_ALIVE_SCRIPTS_DISABLED=.*/KEEP_ALIVE_SCRIPTS_DISABLED=false/I" ${HOME}/.kamehouse/config/kamehouse.cfg
+    sed -i -E "s/^#KEEP_ALIVE_SCRIPTS_DISABLED=.*/KEEP_ALIVE_SCRIPTS_DISABLED=/I" ${KAMEHOUSE_CFG}
+    sed -i -E "s/^KEEP_ALIVE_SCRIPTS_DISABLED=.*/KEEP_ALIVE_SCRIPTS_DISABLED=false/I" ${KAMEHOUSE_CFG}
   else
     log.info "keep alive scripts are ${COL_YELLOW}enabled${COL_DEFAULT_LOG} in kamehouse.cfg. Switching to ${COL_RED}disabled"
-    sed -i -E "s/^#KEEP_ALIVE_SCRIPTS_DISABLED=.*/KEEP_ALIVE_SCRIPTS_DISABLED=/I" ${HOME}/.kamehouse/config/kamehouse.cfg
-    sed -i -E "s/^KEEP_ALIVE_SCRIPTS_DISABLED=.*/KEEP_ALIVE_SCRIPTS_DISABLED=true/I" ${HOME}/.kamehouse/config/kamehouse.cfg
+    sed -i -E "s/^#KEEP_ALIVE_SCRIPTS_DISABLED=.*/KEEP_ALIVE_SCRIPTS_DISABLED=/I" ${KAMEHOUSE_CFG}
+    sed -i -E "s/^KEEP_ALIVE_SCRIPTS_DISABLED=.*/KEEP_ALIVE_SCRIPTS_DISABLED=true/I" ${KAMEHOUSE_CFG}
   fi
   log.info "KEEP_ALIVE_SCRIPTS_DISABLED status in kamehouse.cfg"
   cat ${HOME}/.kamehouse/config/kamehouse.cfg | grep "KEEP_ALIVE_SCRIPTS_DISABLED="
+  ${HOME}/programs/kamehouse-shell/bin/common/keep-alive/keep-alive-status.sh 
 }
 
 main "$@"
