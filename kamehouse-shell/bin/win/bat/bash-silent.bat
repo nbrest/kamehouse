99502fc33f372f81994ddb196a6541d474696666
nbrest <brest.nico@gmail.com>
2025-12-16 19:05:02 +0100

    Fixed groot login on windows

--- kamehouse-shell/bin/win/bat/bash-silent.bat
+++ kamehouse-shell/bin/win/bat/bash-silent.bat
@@ -2,5 +2,5 @@
 rem simplified bash.bat without any logging at all. For use on scripts where I only want returned the main script output
 setlocal EnableDelayedExpansion
 set "arguments=%*"
-"C:\msys64\usr\bin\bash.exe" -i -l !arguments!
+"C:\msys64\usr\bin\bash.exe" !arguments!
 exit
