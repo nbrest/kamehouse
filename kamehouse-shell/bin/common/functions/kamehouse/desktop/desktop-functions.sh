53b924464d51df87ad85b919492178ebcdbd689b
nbrest <brest.nico@gmail.com>
2025-12-16 08:45:10 +0100

    Fixed kill desktop in windows

--- kamehouse-shell/bin/common/functions/kamehouse/desktop/desktop-functions.sh
+++ kamehouse-shell/bin/common/functions/kamehouse/desktop/desktop-functions.sh
@@ -10,7 +10,7 @@ setKameHouseDesktopPid() {
     KAMEHOUSE_DESKTOP_PID=`ps aux | grep "kamehouse_desktop.py" | grep "python" | awk '{print $2}'`
   else
     local POWERSHELL_COMMAND="Get-CimInstance Win32_Process -Filter \"CommandLine like '%kamehouse_desktop.py%'\" | Select-Object Name, ProcessId, CommandLine"
-    KAMEHOUSE_DESKTOP_PID=`powershell.exe -c "${POWERSHELL_COMMAND}" | grep -v "powershell.exe" | grep -v "CommandLine" | grep "python.exe" | awk '{print $2}'`
+    KAMEHOUSE_DESKTOP_PID=`powershell.exe -c "${POWERSHELL_COMMAND}" | grep -v "powershell.exe" | grep -v "CommandLine" | grep "python.exe" | awk '{print $2}' | head -n 1`
   fi
   log.info "KAMEHOUSE_DESKTOP_PID: ${KAMEHOUSE_DESKTOP_PID}"
 }
