0d3e67b9ed2f76692f04a273f3963b449899f36e
nbrest <brest.nico@gmail.com>
2025-12-26 17:57:56 +0100

    Updated aliases

--- kamehouse-shell/bin/common/bashrc/alias.sh
+++ kamehouse-shell/bin/common/bashrc/alias.sh
@@ -1,6 +1,7 @@
+# cd
 alias cd.git="cd ${HOME}/git"
-alias cd.kamehouse='cd ${HOME}/workspace/kamehouse'
 alias cd.git.kamehouse='cd ${HOME}/git/kamehouse'
+alias cd.kamehouse='cd ${HOME}/workspace/kamehouse'
 alias cd.logs='cd ${HOME}/logs'
 alias cd.programs="cd ${HOME}/programs"
 alias cd.tomcat="cd ${HOME}/programs/apache-tomcat"
@@ -8,9 +9,14 @@ alias cd.shell="cd ${HOME}/programs/kamehouse-shell/bin"
 alias cd.webapps="cd ${HOME}/programs/apache-tomcat/webapps"
 alias cd.workspace="cd ${HOME}/workspace"
 
+# edit
 alias edit.kamehouse.cfg="vim ${HOME}/.kamehouse/config/kamehouse.cfg"
 
+# reload
 alias reload.bashrc='source ${HOME}/.bashrc'
+
+# bash
+alias aa='alias'
 
 alias ls='ls --color=auto'
 
@@ -18,14 +24,19 @@ alias CD=cd
 alias LS=ls
 alias PS=ps
 
+# screen
 alias sr='screen -D -RR'
 alias sl='screen -ls'
 alias sd='screen -d'
 alias s='screen'
 
+# sudo
 if [ -f "/usr/sbin/poweroff" ] || [ -f "/sbin/poweroff" ]; then
   alias poweroff='sudo poweroff' 
 fi
 if [ -f "/usr/sbin/reboot" ] || [ -f "/sbin/reboot" ]; then
   alias reboot='sudo reboot'
 fi
+
+# kamehouse-shell scripts shortcuts
+alias kat="keep-alive-toggle.sh"
