29332ba704da211df1c61490a5cc8d6e864f9eef
nbrest <brest.nico@gmail.com>
2026-07-26 18:12:34 +0200

    Moved kamehouse-cmd.sh to kamehouse shell framework

--- kamehouse-cmd/scripts/kamehouse-cmd.sh
+++ kamehouse-cmd/scripts/kamehouse-cmd.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-KAMEHOUSE_CMD_PATH=${HOME}/programs/kamehouse-cmd
+source ${HOME}/programs/kamehouse-shell/bin/functions/kamehouse/kamehouse-functions.sh
+if [ "$?" != "0" ]; then echo "Error importing kamehouse-functions.sh" ; exit 99 ; fi
 
-main() {
+initKameHouseShellEnv() {
+  LOG=DISABLED
+  LOG_PROCESS_TO_FILE=false
+}
+
+initScriptEnv() {
+  KAMEHOUSE_CMD_PATH=${HOME}/programs/kamehouse-cmd
+}
+
+mainProcessPre() {
   setEnvironment
   executeApp "$@"
 }
